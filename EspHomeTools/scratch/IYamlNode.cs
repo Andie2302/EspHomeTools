b22c7140e062e98dd73aa3ad08ecc3ea0f416f5b
Andreas <Andreas.Zwander@gmail.com>
2025-07-17 23:36:42 +0200

    Add IYamlNode interface to EspHomeTools

---
+++ EspHomeTools/scratch/IYamlNode.cs
@@ -0,0 +1,8 @@
+﻿namespace EspHomeTools.scratch;
+
+public interface IYamlNode : IYamlSerializable
+{
+    string? Name { get; set; }
+    string? Comment { get; set; }
+    string? Tag { get; set; }
+}
