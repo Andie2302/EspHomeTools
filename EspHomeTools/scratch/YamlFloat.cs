3fbb6db64e04737d15927d0349cc545349936712
Andreas <Andreas.Zwander@gmail.com>
2025-07-17 23:38:13 +0200

    Add YamlFloat class to EspHomeTools

---
+++ EspHomeTools/scratch/YamlFloat.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace EspHomeTools.scratch;
+
+public class YamlFloat : YamlScalarBase<double>
+{
+    public YamlFloat(double value)
+    {
+        Value = value;
+    }
+
+
+
+
+    protected override string SerializeValue()
+    {
+        if (Value == null) return "null";
+
+        return Value.ToString(CultureInfo.InvariantCulture);
+    }
+}
