e012aa1b86e187de42c7596bef74a18a78d63b5d
Andreas <Andreas.Zwander@gmail.com>
2025-07-17 23:38:31 +0200

    Add YamlScalarBase abstract class to EspHomeTools

---
+++ EspHomeTools/scratch/YamlScalarBase.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+
+namespace EspHomeTools.scratch;
+
+
+
+
+public abstract class YamlScalarBase<TValue> : IYamlScalar<TValue>
+{
+    public TValue? Value { get; set; }
+    public string? Name { get; set; }
+    public string? Comment { get; set; }
+    public string? Tag { get; set; }
+
+
+
+
+    public string ToYaml(int indent = 0)
+    {
+        var sb = new StringBuilder();
+        var prefix = new string(' ', indent);
+
+        if (!string.IsNullOrWhiteSpace(Comment))
+        {
+            sb.Append(prefix).Append("# ").AppendLine(Comment);
+        }
+
+        if (!string.IsNullOrWhiteSpace(Name))
+        {
+            sb.Append(prefix).Append(Name).Append(':');
+
+            if (!string.IsNullOrWhiteSpace(Tag))
+            {
+                sb.Append(' ').Append(Tag);
+            }
+
+            sb.Append(' ');
+        }
+
+        sb.Append(SerializeValue());
+
+        return sb.ToString();
+    }
+
+
+
+
+
+    protected abstract string SerializeValue();
+}
