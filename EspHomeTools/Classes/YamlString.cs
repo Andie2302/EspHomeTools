42312fa16dd642c5eb87001dcf8e1191cb1f1bc9
Andreas <Andreas.Zwander@gmail.com>
2025-07-17 23:51:58 +0200

    Remove redundant blank lines in YamlString class

--- EspHomeTools/Classes/YamlString.cs
+++ EspHomeTools/Classes/YamlString.cs
@@ -12,18 +12,11 @@ public class YamlString : YamlScalarBase<string>
         Value = value;
     }
 
-
-
-
-    protected override string SerializeValue() => NeedsQuoting(Value??="null") ? $"\"{Value.Replace("\"", "\\\"")}\"" : Value;
+    protected override string SerializeValue() => NeedsQuoting(Value ??= "null") ? $"\"{Value.Replace("\"", "\\\"")}\"" : Value;
 
     private bool NeedsQuoting(string str)
     {
         if (string.IsNullOrEmpty(str)) return true;
-
-        return str.Any(c => SpecialYamlChars.Contains(c)) ||
-               double.TryParse(str, out _) ||
-               bool.TryParse(str, out _) ||
-               str.Equals("null", StringComparison.OrdinalIgnoreCase);
+        return str.Any(c => SpecialYamlChars.Contains(c)) || double.TryParse(str, out _) || bool.TryParse(str, out _) || str.Equals("null", StringComparison.OrdinalIgnoreCase);
     }
 }
