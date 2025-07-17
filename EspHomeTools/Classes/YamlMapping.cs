f0de314fab30c2376b3bf8fe2c218c47400b2968
Andreas <Andreas.Zwander@gmail.com>
2025-07-17 23:47:15 +0200

    Rename and move YamlMapping class to Classes folder

--- EspHomeTools/Classes/YamlMapping.cs
+++ EspHomeTools/Classes/YamlMapping.cs
@@ -2,8 +2,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using EspHomeTools.Interfaces;
 
-namespace EspHomeTools.scratch;
+namespace EspHomeTools.Classes;
 
 public class YamlMapping : IYamlMapping
 {
