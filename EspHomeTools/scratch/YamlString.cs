18140a6ceaeb3588589a6d0c08f5d43baef7e283
Andreas <Andreas.Zwander@gmail.com>
2025-07-17 23:39:22 +0200

    Add YamlString class to EspHomeTools

---
+++ EspHomeTools/scratch/YamlString.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace EspHomeTools.scratch;
+
+public class YamlString : YamlScalarBase<string>
+{
+    private readonly static char[] SpecialYamlChars = [':', '{', '}', '[', ']', ',', '&', '*', '#', '?', '|', '-', '<', '>', '!', '%', '@', '`'];
+
+    public YamlString(string value)
+    {
+        Value = value;
+    }
+
+
+
+
+    protected override string SerializeValue() => NeedsQuoting(Value??="null") ? $"\"{Value.Replace("\"", "\\\"")}\"" : Value;
+
+    private bool NeedsQuoting(string str)
+    {
+        if (string.IsNullOrEmpty(str)) return true;
+
+        return str.Any(c => SpecialYamlChars.Contains(c)) ||
+               double.TryParse(str, out _) ||
+               bool.TryParse(str, out _) ||
+               str.Equals("null", StringComparison.OrdinalIgnoreCase);
+    }
+}
