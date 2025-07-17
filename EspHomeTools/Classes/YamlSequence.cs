74af213cc69f17b7edd8983a4a413448daae9139
Andreas <Andreas.Zwander@gmail.com>
2025-07-17 23:47:27 +0200

    Rename and move YamlSequence class to Classes folder

--- EspHomeTools/Classes/YamlSequence.cs
+++ EspHomeTools/Classes/YamlSequence.cs
@@ -3,8 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using EspHomeTools.Interfaces;
 
-namespace EspHomeTools.scratch;
+namespace EspHomeTools.Classes;
 
 public class YamlSequence : IYamlSequence
 {
