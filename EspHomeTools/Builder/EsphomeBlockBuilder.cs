73f3cc42f1b28fa78c5f4c59457cc88aa5122866
Andreas <Andreas.Zwander@gmail.com>
2025-07-18 02:35:57 +0200

    Add `EsphomeBlockBuilder` class for constructing ESPHome YAML blocks

---
+++ EspHomeTools/Builder/EsphomeBlockBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using EspHomeTools.Classes;
+using EspHomeTools.Interfaces;
+
+namespace EspHomeTools.Builder;
+
+public class EsphomeBlockBuilder
+{
+    private readonly YamlMapping _block = new();
+
+    public EsphomeBlockBuilder WithName(string name)
+    {
+        _block["name"] = new YamlString(name);
+        return this;
+    }
+
+    public EsphomeBlockBuilder WithComment(string comment)
+    {
+        if (_block.TryGetValue("name", out var nameNode))
+        {
+            nameNode.Comment = comment;
+        }
+
+        return this;
+    }
+
+    internal IYamlMapping Build()
+    {
+        if (!_block.ContainsKey("name"))
+        {
+            throw new InvalidOperationException("Der Name im 'esphome'-Block ist erforderlich. Benutze die WithName()-Methode.");
+        }
+
+        return _block;
+    }
+}
