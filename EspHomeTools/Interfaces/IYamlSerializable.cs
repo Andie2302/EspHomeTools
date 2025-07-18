28ddb2baaa878167e257f138ef708d5f34ed6212
Andreas <Andreas.Zwander@gmail.com>
2025-07-18 14:07:27 +0200

    Add XML documentation to `IYamlSerializable` interface for improved clarity and context

--- EspHomeTools/Interfaces/IYamlSerializable.cs
+++ EspHomeTools/Interfaces/IYamlSerializable.cs
@@ -1,6 +1,18 @@
 ﻿namespace EspHomeTools.Interfaces;
 
+/// <summary>
+/// Represents an object that can be serialized into a YAML string format.
+/// Provides functionality to convert the implementing object to its
+/// equivalent YAML representation with optional indentation.
+/// </summary>
 public interface IYamlSerializable
 {
+    /// Converts the object to a YAML string representation.
+    /// <param name="indent">
+    /// The number of spaces to prepend to each line for indentation. Default is 0.
+    /// </param>
+    /// <returns>
+    /// A string representing the YAML formatted content of the object.
+    /// </returns>
     string ToYaml(int indent = 0);
 }
