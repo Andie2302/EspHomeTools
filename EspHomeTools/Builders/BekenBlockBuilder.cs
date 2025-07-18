61598e0e4010c3062a45fb7c23a331c9e5dfd1ef
Andreas <Andreas.Zwander@gmail.com>
2025-07-19 00:24:17 +0200

    Add `BekenBlockBuilder` class for building `bk72xx` blocks with board validation

---
+++ EspHomeTools/Builders/BekenBlockBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using EspHomeTools.Classes.Scalars;
+using EspHomeTools.Classes.Structures;
+using EspHomeTools.Interfaces;
+
+namespace EspHomeTools.Builders;
+
+public class BekenBlockBuilder
+{
+    private readonly YamlMapping _block = new();
+
+    public BekenBlockBuilder WithBoard(string board)
+    {
+        _block["board"] = new YamlString(board);
+        return this;
+    }
+
+    internal IYamlMapping Build()
+    {
+        if (!_block.ContainsKey("board"))
+        {
+            throw new InvalidOperationException("Für den 'bk72xx'-Block muss ein Board mit WithBoard() angegeben werden.");
+        }
+
+        return _block;
+    }
+}
