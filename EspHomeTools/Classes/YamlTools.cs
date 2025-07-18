c8ddffb4e10bc90bd3adced4a3dee5ee34ef8f81
Andreas <Andreas.Zwander@gmail.com>
2025-07-18 13:58:25 +0200

    Add `YamlTools` class with `NeedsQuoting` helper for YAML value processing

---
+++ EspHomeTools/Classes/YamlTools.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace EspHomeTools.Classes;
+
+public static class YamlTools
+{
+    public readonly static char[] SpecialYamlChars = [':', '{', '}', '[', ']', ',', '&', '*', '#', '?', '|', '-', '<', '>', '!', '%', '@', '`'];
+    public static bool NeedsQuoting(string? str)
+    {
+        if (str==null||string.IsNullOrWhiteSpace(str)) return true;
+        return str.Any(c => SpecialYamlChars.Contains(c)) || double.TryParse(str, out _) || bool.TryParse(str, out _) || str.Equals("null", StringComparison.OrdinalIgnoreCase);
+    }
+}
