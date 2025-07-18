0bfb8228a49c7a16dee32ca1e883610fb733bd8e
Andreas <Andreas.Zwander@gmail.com>
2025-07-18 14:07:39 +0200

    Add XML documentation to `YamlInt` for improved clarity and context

--- EspHomeTools/Classes/YamlInt.cs
+++ EspHomeTools/Classes/YamlInt.cs
@@ -1,11 +1,26 @@
 ﻿namespace EspHomeTools.Classes;
 
+/// <summary>
+/// Represents a YAML scalar integer node, providing functionality to serialize integer values into YAML format.
+/// </summary>
+/// <remarks>
+/// This class is a specific implementation of <see cref="YamlScalar{TValue}"/> for handling integer values.
+/// It supports serialization to YAML with optional naming, commenting, and tagging.
+/// </remarks>
 public class YamlInt : YamlScalar<int>
 {
+    /// Represents a scalar integer value in a YAML structure. Inherits from the generic `YamlScalar<int>` class,
+    /// providing type-specific behavior for serializing and managing integer scalar values.
     public YamlInt(int value)
     {
         Value = value;
     }
 
+    /// <summary>
+    /// Serializes the scalar value of the YAML node into its string representation.
+    /// </summary>
+    /// <returns>
+    /// The string representation of the scalar value.
+    /// </returns>
     protected override string SerializeValue() => Value.ToString();
 }
