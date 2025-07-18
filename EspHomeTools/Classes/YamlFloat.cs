18fcd0d39e5a573453a97fae31e1655411e9599e
Andreas <Andreas.Zwander@gmail.com>
2025-07-18 14:07:33 +0200

    Add XML documentation to `YamlFloat` for improved clarity and context

--- EspHomeTools/Classes/YamlFloat.cs
+++ EspHomeTools/Classes/YamlFloat.cs
@@ -2,12 +2,24 @@
 
 namespace EspHomeTools.Classes;
 
+/// <summary>
+/// Represents a YAML scalar value of type double, providing functionality for serialization and manipulation
+/// specific to floating-point numbers.
+/// </summary>
 public class YamlFloat : YamlScalar<double>
 {
+    /// Represents a YAML float scalar value.
+    /// This class is derived from `YamlScalar<double>`
+    /// and provides a mechanism to handle and serialize
+    /// double-precision floating-point numbers as YAML scalar values.
     public YamlFloat(double value)
     {
         Value = value;
     }
 
+    /// Serializes the value of the current YAML scalar object into a string representation.
+    /// <returns>
+    /// A string representing the serialized value of the current YAML scalar object.
+    /// </returns>
     protected override string SerializeValue() => Value.ToString(CultureInfo.InvariantCulture);
 }
