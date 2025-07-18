5b20a9b30cc3d9832ba147875a3696f8049ff28e
Andreas <Andreas.Zwander@gmail.com>
2025-07-19 00:55:33 +0200

    Add `AccessPointBlockBuilder` class and `WithAccessPoint` method to `WifiBlockBuilder` for configuring access points

--- EspHomeTools/Builders/WifiBlockBuilder.cs
+++ EspHomeTools/Builders/WifiBlockBuilder.cs
@@ -5,6 +5,38 @@ using EspHomeTools.Interfaces;
 
 namespace EspHomeTools.Builders;
 
+public class AccessPointBlockBuilder
+{
+    private readonly YamlMapping _block = new();
+
+    public AccessPointBlockBuilder WithSsid(string ssid)
+    {
+        _block["ssid"] = new YamlString(ssid);
+        return this;
+    }
+
+    public AccessPointBlockBuilder WithPassword(string password)
+    {
+        _block["password"] = new YamlString(password);
+        return this;
+    }
+
+    public AccessPointBlockBuilder WithPassword(YamlSecret password)
+    {
+        _block["password"] = password;
+        return this;
+    }
+
+    internal IYamlMapping Build()
+    {
+        if (!_block.ContainsKey("ssid"))
+        {
+            throw new InvalidOperationException("Die SSID für den Access Point (AP) ist erforderlich.");
+        }
+
+        return _block;
+    }
+}
 public class WifiBlockBuilder
 {
     private readonly YamlMapping _block = new();
@@ -15,9 +47,29 @@ public class WifiBlockBuilder
         return this;
     }
 
+    public WifiBlockBuilder WithSsid(YamlSecret ssid)
+    {
+        _block["ssid"] = ssid;
+        return this;
+    }
+
     public WifiBlockBuilder WithPassword(string password)
     {
         _block["password"] = new YamlString(password);
+        return this;
+    }
+
+    public WifiBlockBuilder WithPassword(YamlSecret password)
+    {
+        _block["password"] = password;
+        return this;
+    }
+
+    public WifiBlockBuilder WithAccessPoint(Action<AccessPointBlockBuilder> configurator)
+    {
+        var builder = new AccessPointBlockBuilder();
+        configurator(builder);
+        _block["ap"] = builder.Build();
         return this;
     }
 
@@ -29,10 +81,5 @@ public class WifiBlockBuilder
         }
 
         return _block;
-    }
-    public WifiBlockBuilder WithPassword(YamlSecret password)
-    {
-        _block["password"] = password;
-        return this;
     }
 }
