18d2a019883e6cfe72e733c8f5f29e2ae8433999
Andreas <Andreas.Zwander@gmail.com>
2025-07-18 13:57:50 +0200

    Add `DhtSensorBuilder` class for constructing DHT sensor configurations in YAML

---
+++ EspHomeTools/Builder/DhtSensorBuilder.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using EspHomeTools.Classes;
+using EspHomeTools.Interfaces;
+
+namespace EspHomeTools.Builder;
+
+public class DhtSensorBuilder
+{
+    private readonly YamlMapping _config = new();
+
+    public DhtSensorBuilder()
+    {
+        _config["platform"] = new YamlString("dht");
+    }
+
+    public DhtSensorBuilder UsePin(string pin)
+    {
+        _config["pin"] = new YamlString(pin);
+        return this;
+    }
+
+    public DhtSensorBuilder WithTemperature(string name)
+    {
+        _config["temperature"] = new YamlMapping { { "name", new YamlString(name) } };
+        return this;
+    }
+
+    public DhtSensorBuilder WithHumidity(string name)
+    {
+        _config["humidity"] = new YamlMapping { { "name", new YamlString(name) } };
+        return this;
+    }
+
+    public DhtSensorBuilder WithUpdateInterval(string interval)
+    {
+        _config["update_interval"] = new YamlString(interval);
+        return this;
+    }
+
+    public DhtSensorBuilder WithModel(string model)
+    {
+        _config["model"] = new YamlString(model);
+        return this;
+    }
+
+    internal IYamlMapping Build()
+    {
+        if (!_config.ContainsKey("pin"))
+        {
+            throw new InvalidOperationException("Ein Pin muss für den DHT-Sensor mit UsePin() angegeben werden.");
+        }
+
+        if (!_config.ContainsKey("temperature") && !_config.ContainsKey("humidity"))
+        {
+            throw new InvalidOperationException("Für einen DHT-Sensor muss mindestens Temperatur (WithTemperature) oder Feuchtigkeit (WithHumidity) konfiguriert werden.");
+        }
+
+        return _config;
+    }
+}
