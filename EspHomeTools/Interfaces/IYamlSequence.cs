bdf1e99511761f695c0b17361ebe18fdea8bc3b0
Andreas <Andreas.Zwander@gmail.com>
2025-07-18 14:07:22 +0200

    Add XML documentation to `IYamlSequence` interface for improved clarity and context

--- EspHomeTools/Interfaces/IYamlSequence.cs
+++ EspHomeTools/Interfaces/IYamlSequence.cs
@@ -2,4 +2,17 @@
 
 namespace EspHomeTools.Interfaces;
 
+/// <summary>
+/// Represents a YAML sequence structure as defined in the YAML specification.
+/// </summary>
+/// <remarks>
+/// An implementation of this interface provides functionality to manage a list of YAML nodes
+/// in a sequential order. It extends both <see cref="IYamlStructure"/> and <see cref="IList{T}"/>,
+/// where T is <see cref="IYamlNode"/>. This allows the sequence to behave as a collection of
+/// YAML nodes that can be indexed, enumerated, and manipulated while also supporting YAML-specific
+/// serialization and structural behavior.
+/// </remarks>
+/// <seealso cref="IYamlStructure"/>
+/// <seealso cref="IYamlNode"/>
+/// <seealso cref="IYamlSerializable"/>
 public interface IYamlSequence : IYamlStructure, IList<IYamlNode> { }
