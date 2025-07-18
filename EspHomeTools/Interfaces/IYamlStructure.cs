1a0a8d3e7d3f8fc99897b8cb56542083547aaa37
Andreas <Andreas.Zwander@gmail.com>
2025-07-18 14:06:56 +0200

    Add XML documentation to `IYamlStructure` interface for better clarity and context

--- EspHomeTools/Interfaces/IYamlStructure.cs
+++ EspHomeTools/Interfaces/IYamlStructure.cs
@@ -1,3 +1,7 @@
 ﻿namespace EspHomeTools.Interfaces;
 
+/// <summary>
+/// Represents a YAML structure element, which can either be a sequence or a mapping.
+/// It provides a base interface for structured YAML data and extends the functionality of <see cref="IYamlNode"/>.
+/// </summary>
 public interface IYamlStructure : IYamlNode { }
