5430c458969e085cf3d5d6cd6d0121cc8bef1f59
Andreas <Andreas.Zwander@gmail.com>
2025-07-19 00:24:25 +0200

    Add `Esp32BlockBuilder` class for building `esp32` blocks with board validation

---
+++ EspHomeTools/Builders/Esp32BlockBuilder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using EspHomeTools.Classes.Scalars;
+using EspHomeTools.Classes.Structures;
+using EspHomeTools.Interfaces;
+
+namespace EspHomeTools.Builders;
+
+public class Esp32BlockBuilder
+{
+    private readonly YamlMapping _block = new();
+
+    public Esp32BlockBuilder WithBoard(string board)
+    {
+        _block["board"] = new YamlString(board);
+        return this;
+    }
+
+    public Esp32BlockBuilder WithFramework(string framework)
+    {
+        _block["framework"] = new YamlString(framework);
+        return this;
+    }
+
+    internal IYamlMapping Build()
+    {
+        if (!_block.ContainsKey("board"))
+        {
+            throw new InvalidOperationException("Für den 'esp32'-Block muss ein Board mit WithBoard() angegeben werden.");
+        }
+
+        return _block;
+    }
+}
