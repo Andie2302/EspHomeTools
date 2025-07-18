e9e925d2da31ff02c860ac2d774f19b6bbc58853
Andreas <Andreas.Zwander@gmail.com>
2025-07-19 00:24:36 +0200

    Add `Rp2040BlockBuilder` class for building `rp2040` blocks with board validation

---
+++ EspHomeTools/Builders/Rp2040BlockBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using EspHomeTools.Classes.Scalars;
+using EspHomeTools.Classes.Structures;
+using EspHomeTools.Interfaces;
+
+namespace EspHomeTools.Builders;
+
+public class Rp2040BlockBuilder
+{
+    private readonly YamlMapping _block = new();
+
+    public Rp2040BlockBuilder WithBoard(string board)
+    {
+        _block["board"] = new YamlString(board);
+        return this;
+    }
+
+    internal IYamlMapping Build()
+    {
+        if (!_block.ContainsKey("board"))
+        {
+            throw new InvalidOperationException("Für den 'rp2040'-Block muss ein Board mit WithBoard() angegeben werden.");
+        }
+
+        return _block;
+    }
+}
