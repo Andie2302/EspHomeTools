e660fe3c616e588f72f07615cfa5cb1c3538d86d
Andreas <Andreas.Zwander@gmail.com>
2025-07-18 21:34:44 +0200

    Add unit tests for `Scalar` classes

---
+++ TestEspHomeTools/ScalarTests.cs
@@ -0,0 +1,62 @@
+﻿using EspHomeTools.Classes.Scalars;
+
+namespace TestEspHomeTools;
+
+
+[TestClass]
+public class ScalarTests
+{
+    [TestMethod]
+    public void YamlInt_ToYaml_ReturnsCorrectString()
+    {
+        var yamlInt = new YamlInt(42);
+        var expected = "42";
+        var actual = yamlInt.ToYaml();
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void YamlFloat_ToYaml_ReturnsCorrectStringWithDot()
+    {
+        var yamlFloat = new YamlFloat(123.45);
+        var expected = "123.45";
+        var actual = yamlFloat.ToYaml();
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void YamlBool_ToYaml_TrueReturnsLowercaseString()
+    {
+        var yamlBool = new YamlBool(true);
+        var expected = "true";
+        var actual = yamlBool.ToYaml();
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void YamlString_ToYaml_SimpleStringIsNotQuoted()
+    {
+        var yamlString = new YamlString("hello world");
+        var expected = "hello world";
+        var actual = yamlString.ToYaml();
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void YamlString_ToYaml_StringWithSpecialCharIsQuoted()
+    {
+        var yamlString = new YamlString("hello: world");
+        var expected = "\"hello: world\"";
+        var actual = yamlString.ToYaml();
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void YamlString_ToYaml_NumericStringIsQuoted()
+    {
+        var yamlString = new YamlString("123");
+        var expected = "\"123\"";
+        var actual = yamlString.ToYaml();
+        Assert.AreEqual(expected, actual);
+    }
+}
