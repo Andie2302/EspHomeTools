a16fba262b41b723b7d36c89162312ff1a2a8513
Andreas <Andreas.Zwander@gmail.com>
2025-07-18 02:36:11 +0200

    Add `WifiBlockBuilder` class for constructing WiFi configuration blocks

---
+++ EspHomeTools/Builder/WifiBlockBuilder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using EspHomeTools.Classes;
+using EspHomeTools.Interfaces;
+
+namespace EspHomeTools.Builder;
+
+public class WifiBlockBuilder
+{
+    private readonly YamlMapping _block = new();
+
+    public WifiBlockBuilder WithSsid(string ssid)
+    {
+        _block["ssid"] = new YamlString(ssid);
+        return this;
+    }
+
+    public WifiBlockBuilder WithPassword(string password)
+    {
+        _block["password"] = new YamlString(password);
+        return this;
+    }
+
+    internal IYamlMapping Build()
+    {
+        if (!_block.ContainsKey("ssid") || !_block.ContainsKey("password"))
+        {
+            throw new InvalidOperationException("SSID und Passwort sind im 'wifi'-Block erforderlich.");
+        }
+
+        return _block;
+    }
+}
