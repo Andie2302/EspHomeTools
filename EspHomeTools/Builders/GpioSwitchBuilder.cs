b434b86603162ca3c9c27c2caac60c93719e90a5
Andreas <Andreas.Zwander@gmail.com>
2025-07-19 14:54:15 +0200

    Add overloads in `GpioSwitchBuilder` for conditional secret usage

--- EspHomeTools/Builders/GpioSwitchBuilder.cs
+++ EspHomeTools/Builders/GpioSwitchBuilder.cs
@@ -20,11 +20,27 @@ public class GpioSwitchBuilder
         return this;
     }
 
+    public GpioSwitchBuilder UsePin(YamlSecret pin)
+    {
+        _config["pin"] = pin;
+        return this;
+    }
+
+    public GpioSwitchBuilder UsePin(string pin, bool isSecret) => isSecret ? UsePin(new YamlSecret(pin)) : UsePin(pin);
+
     public GpioSwitchBuilder WithName(string name)
     {
         _config["name"] = new YamlString(name);
         return this;
     }
+
+    public GpioSwitchBuilder WithName(YamlSecret name)
+    {
+        _config["name"] = name;
+        return this;
+    }
+
+    public GpioSwitchBuilder WithName(string name, bool isSecret) => isSecret ? WithName(new YamlSecret(name)) : WithName(name);
 
     public GpioSwitchBuilder WithId(string id)
     {
@@ -32,11 +48,27 @@ public class GpioSwitchBuilder
         return this;
     }
 
+    public GpioSwitchBuilder WithId(YamlSecret id)
+    {
+        _config["id"] = id;
+        return this;
+    }
+
+    public GpioSwitchBuilder WithId(string id, bool isSecret) => isSecret ? WithId(new YamlSecret(id)) : WithId(id);
+
     public GpioSwitchBuilder WithIcon(string icon)
     {
         _config["icon"] = new YamlString(icon);
         return this;
     }
+
+    public GpioSwitchBuilder WithIcon(YamlSecret icon)
+    {
+        _config["icon"] = icon;
+        return this;
+    }
+
+    public GpioSwitchBuilder WithIcon(string icon, bool isSecret) => isSecret ? WithIcon(new YamlSecret(icon)) : WithIcon(icon);
 
     internal IYamlMapping Build()
     {
