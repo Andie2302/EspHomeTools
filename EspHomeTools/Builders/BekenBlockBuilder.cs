d91f691a24b72e8702e9be3cf07a33ad28f7991d
Andreas <Andreas.Zwander@gmail.com>
2025-07-19 15:13:22 +0200

    Add `BekenBlockBuilder` with secret-aware board handling

---
+++ EspHomeTools/Builders/BekenBlockBuilder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using EspHomeTools.Classes.Scalars;
+using EspHomeTools.Classes.Structures;
+using EspHomeTools.Interfaces;
+
+namespace EspHomeTools.Builders;
+
+public class BekenBlockBuilder
+{
+    private readonly YamlMapping _block = new();
+
+    public BekenBlockBuilder WithBoard(string board)
+    {
+        _block["board"] = new YamlString(board);
+        return this;
+    }
+
+    public BekenBlockBuilder WithBoard(YamlSecret board)
+    {
+        _block["board"] = board;
+        return this;
+    }
+
+    public BekenBlockBuilder WithBoard(string board, bool isSecret) => isSecret ? WithBoard(new YamlSecret(board)) : WithBoard(board);
+
+    internal IYamlMapping Build()
+    {
+        if (!_block.ContainsKey("board"))
+        {
+            throw new InvalidOperationException("Für den 'bk72xx'-Block muss ein Board mit WithBoard() angegeben werden.");
+        }
+
+        return _block;
+    }
+}
