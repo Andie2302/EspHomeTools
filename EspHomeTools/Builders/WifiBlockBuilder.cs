f2d2da2977216d58b58732945d0ddbe55dc6f0d5
Andreas <Andreas.Zwander@gmail.com>
2025-07-19 15:11:05 +0200

    Remove `AccessPointBlockBuilder` and add secret-aware SSID handling in `WifiBlockBuilder`

--- EspHomeTools/Builders/WifiBlockBuilder.cs
+++ EspHomeTools/Builders/WifiBlockBuilder.cs
@@ -5,40 +5,6 @@ using EspHomeTools.Interfaces;
 
 namespace EspHomeTools.Builders;
 
-public class AccessPointBlockBuilder
-{
-    private readonly YamlMapping _block = new();
-
-    public AccessPointBlockBuilder WithSsid(string ssid)
-    {
-        _block["ssid"] = new YamlString(ssid);
-        return this;
-    }
-
-    public AccessPointBlockBuilder WithPassword(string password)
-    {
-        _block["password"] = new YamlString(password);
-        return this;
-    }
-
-    public AccessPointBlockBuilder WithPassword(string password, bool isSecret) => isSecret ? WithPassword(new YamlSecret(password)) : WithPassword(password);
-
-    public AccessPointBlockBuilder WithPassword(YamlSecret password)
-    {
-        _block["password"] = password;
-        return this;
-    }
-
-    internal IYamlMapping Build()
-    {
-        if (!_block.ContainsKey("ssid"))
-        {
-            throw new InvalidOperationException("Die SSID für den Access Point (AP) ist erforderlich.");
-        }
-
-        return _block;
-    }
-}
 public class WifiBlockBuilder
 {
     private readonly YamlMapping _block = new();
@@ -54,6 +20,8 @@ public class WifiBlockBuilder
         _block["ssid"] = ssid;
         return this;
     }
+
+    public WifiBlockBuilder WithSsid(string ssid, bool isSecret) => isSecret ? WithSsid(new YamlSecret(ssid)) : WithSsid(ssid);
 
     public WifiBlockBuilder WithPassword(string password)
     {
