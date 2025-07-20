2fb3f664a042e2500d9e86ab8711eb3199d09cf8
Andreas <Andreas.Zwander@gmail.com>
2025-07-21 01:19:32 +0200

    Add `YamlBlockComparer` to enable custom sorting of YAML nodes

---
+++ EspHomeTools/Classes/Collections/YamlBlockComparer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using EspHomeTools.Interfaces;
+
+namespace EspHomeTools.Classes.Collections;
+
+public class YamlBlockComparer : IComparer<KeyValuePair<string, IYamlNode>>
+{
+    private readonly List<string> _customOrder;
+    public YamlBlockComparer(List<string> customOrder)
+    {
+        _customOrder = customOrder;
+    }
+    public int Compare(KeyValuePair<string, IYamlNode> x, KeyValuePair<string, IYamlNode> y)
+    {
+        var indexX = _customOrder.FindIndex(key => key.Equals(x.Key, StringComparison.OrdinalIgnoreCase));
+        var indexY = _customOrder.FindIndex(key => key.Equals(y.Key, StringComparison.OrdinalIgnoreCase));
+        if (indexX == -1) indexX = int.MaxValue;
+        if (indexY == -1) indexY = int.MaxValue;
+        return indexX == indexY ? string.Compare(x.Key, y.Key, StringComparison.Ordinal) : indexX.CompareTo(indexY);
+    }
+
+}
