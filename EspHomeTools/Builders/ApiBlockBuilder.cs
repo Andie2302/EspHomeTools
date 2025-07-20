0b3076304c871f4233ee7d8539ede331761ebd6a
Andreas <Andreas.Zwander@gmail.com>
2025-07-20 02:59:55 +0200

    Add `ApiBlockBuilder` for streamlined API configuration creation with support for encryption, password handling, and comments

---
+++ EspHomeTools/Builders/ApiBlockBuilder.cs
@@ -0,0 +1,53 @@
+﻿using EspHomeTools.Classes.Scalars;
+using EspHomeTools.Classes.Structures;
+using EspHomeTools.Interfaces;
+
+namespace EspHomeTools.Builders;
+
+public class ApiBlockBuilder
+{
+    private readonly YamlMapping _block = new();
+
+    public ApiBlockBuilder WithPassword(string password)
+    {
+        _block["password"] = new YamlString(password);
+        return this;
+    }
+
+    public ApiBlockBuilder WithPassword(YamlSecret password)
+    {
+        _block["password"] = password;
+        return this;
+    }
+
+    public ApiBlockBuilder WithPassword(string password, bool isSecret) => isSecret ? WithPassword(new YamlSecret(password)) : WithPassword(password);
+
+    public ApiBlockBuilder WithEncryptionKey(string key)
+    {
+        var encryptionMapping = new YamlMapping { { "key", new YamlString(key) } };
+        _block["encryption"] = encryptionMapping;
+        return this;
+    }
+
+    public ApiBlockBuilder WithEncryptionKey(YamlSecret key)
+    {
+        var encryptionMapping = new YamlMapping { { "key", key } };
+        _block["encryption"] = encryptionMapping;
+        return this;
+    }
+
+    public ApiBlockBuilder WithEncryptionKey(string key, bool isSecret) => isSecret ? WithEncryptionKey(new YamlSecret(key)) : WithEncryptionKey(key);
+
+    public ApiBlockBuilder WithCommentOn(string key, string comment)
+    {
+        if (_block.TryGetValue(key, out var node))
+            node.Comment = comment;
+
+        return this;
+    }
+
+    internal IYamlMapping Build()
+    {
+        return _block;
+    }
+}
