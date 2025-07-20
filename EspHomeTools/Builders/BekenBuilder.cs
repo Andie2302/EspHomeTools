97c50907b8e65480170d5ab62ac4883983bf2476
Andreas <Andreas.Zwander@gmail.com>
2025-07-20 20:52:55 +0200

    Rename `BekenBlockBuilder` to `BekenBuilder` for naming consistency.

--- EspHomeTools/Builders/BekenBuilder.cs
+++ EspHomeTools/Builders/BekenBuilder.cs
@@ -5,25 +5,25 @@ using EspHomeTools.Interfaces;
 
 namespace EspHomeTools.Builders;
 
-public class BekenBlockBuilder
+public class BekenBuilder
 {
     private readonly YamlMapping _block = new();
 
-    public BekenBlockBuilder WithBoard(string board)
+    public BekenBuilder WithBoard(string board)
     {
         _block["board"] = new YamlString(board);
         return this;
     }
 
-    public BekenBlockBuilder WithBoard(YamlSecret board)
+    public BekenBuilder WithBoard(YamlSecret board)
     {
         _block["board"] = board;
         return this;
     }
 
-    public BekenBlockBuilder WithBoard(string board, bool isSecret) => isSecret ? WithBoard(new YamlSecret(board)) : WithBoard(board);
+    public BekenBuilder WithBoard(string board, bool isSecret) => isSecret ? WithBoard(new YamlSecret(board)) : WithBoard(board);
 
-    public BekenBlockBuilder WithCommentOn(string key, string comment)
+    public BekenBuilder WithCommentOn(string key, string comment)
     {
         if (_block.TryGetValue(key, out var node))
             node.Comment = comment;
