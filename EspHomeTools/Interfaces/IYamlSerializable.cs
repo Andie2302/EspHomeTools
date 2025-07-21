a205a078ae2121943b3d4c35b979938d63bcf73c
Andreas <Andreas.Zwander@gmail.com>
2025-07-21 23:31:50 +0200

    Refactor `IYamlSerializable` interface: remove excessive XML documentation to improve clarity and maintain consistency across interfaces.

--- EspHomeTools/Interfaces/IYamlSerializable.cs
+++ EspHomeTools/Interfaces/IYamlSerializable.cs
@@ -1,16 +1,6 @@
 ﻿namespace EspHomeTools.Interfaces;
 
-/// <summary>
-/// Defines an object capable of serializing itself into a YAML formatted string.
-/// Provides methods to represent the object's data structure in YAML syntax.
-/// </summary>
 public interface IYamlSerializable
 {
-    /// <summary>
-    /// Converts the object to a YAML string representation.
-    /// </summary>
-    /// <param name="name">The name (key) to be used for this node when it's part of a mapping. Can be null.</param>
-    /// <param name="indent">The number of spaces to prepend to each line for indentation. Default is 0.</param>
-    /// <returns>A string representing the YAML formatted content of the object.</returns>
     string ToYaml(int indent, string? name);
 }
