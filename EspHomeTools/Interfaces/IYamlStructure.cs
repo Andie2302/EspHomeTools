78b1e920c4309c8462128d6b1815e22e8c862a78
Andreas <Andreas.Zwander@gmail.com>
2025-07-21 23:31:55 +0200

    Refactor `IYamlStructure` interface: remove excessive XML documentation to improve clarity and maintain consistency across interfaces.

--- EspHomeTools/Interfaces/IYamlStructure.cs
+++ EspHomeTools/Interfaces/IYamlStructure.cs
@@ -1,7 +1,3 @@
 ﻿namespace EspHomeTools.Interfaces;
 
-/// <summary>
-/// Defines the base interface for YAML structures, which can include sequence and mapping implementations.
-/// Serves as a foundation for more specific YAML node structures, extending the <see cref="IYamlNode"/> interface.
-/// </summary>
 public interface IYamlStructure : IYamlNode { }
