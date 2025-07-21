737da10611bb92c646ceb5706833f1fabb75d1ca
Andreas <Andreas.Zwander@gmail.com>
2025-07-21 03:12:52 +0200

    Refactor `ApiBuilder` by extracting `CreateYamlNode` and `CreateEncryptionMapping` helper methods for cleaner and more maintainable code.

--- EspHomeTools/Builders/ApiBuilder.cs
+++ EspHomeTools/Builders/ApiBuilder.cs
@@ -10,7 +10,7 @@ public class ApiBuilder
 
     public ApiBuilder WithPassword(string password)
     {
-        _block["password"] = new YamlString(password);
+        _block["password"] = CreateYamlNode(password, false);
         return this;
     }
 
@@ -20,23 +20,29 @@ public class ApiBuilder
         return this;
     }
 
-    public ApiBuilder WithPassword(string password, bool isSecret) => isSecret ? WithPassword(new YamlSecret(password)) : WithPassword(password);
+    public ApiBuilder WithPassword(string password, bool isSecret)
+    {
+        _block["password"] = CreateYamlNode(password, isSecret);
+        return this;
+    }
 
     public ApiBuilder WithEncryptionKey(string key)
     {
-        var encryptionMapping = new YamlMapping { { "key", new YamlString(key) } };
-        _block["encryption"] = encryptionMapping;
+        _block["encryption"] = CreateEncryptionMapping(key, false);
         return this;
     }
 
     public ApiBuilder WithEncryptionKey(YamlSecret key)
     {
-        var encryptionMapping = new YamlMapping { { "key", key } };
-        _block["encryption"] = encryptionMapping;
+        _block["encryption"] = CreateEncryptionMapping(key);
         return this;
     }
 
-    public ApiBuilder WithEncryptionKey(string key, bool isSecret) => isSecret ? WithEncryptionKey(new YamlSecret(key)) : WithEncryptionKey(key);
+    public ApiBuilder WithEncryptionKey(string key, bool isSecret)
+    {
+        _block["encryption"] = CreateEncryptionMapping(key, isSecret);
+        return this;
+    }
 
     public ApiBuilder WithCommentOn(string key, string comment)
     {
@@ -49,5 +55,20 @@ public class ApiBuilder
     internal IYamlMapping Build()
     {
         return _block;
+    }
+
+    private static IYamlNode CreateYamlNode(string value, bool isSecret)
+    {
+        return isSecret ? new YamlSecret(value) : new YamlString(value);
+    }
+
+    private static YamlMapping CreateEncryptionMapping(string key, bool isSecret)
+    {
+        return new YamlMapping { { "key", CreateYamlNode(key, isSecret) } };
+    }
+
+    private static YamlMapping CreateEncryptionMapping(YamlSecret key)
+    {
+        return new YamlMapping { { "key", key } };
     }
 }
