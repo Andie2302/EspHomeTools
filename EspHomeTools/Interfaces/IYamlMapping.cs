afe4e7a81dfe3046a539c85e831507197412e844
Andreas <Andreas.Zwander@gmail.com>
2025-07-21 23:31:29 +0200

    Refactor `IYamlMapping` interface: remove excessive XML documentation to improve clarity and maintain consistency across interfaces.

--- EspHomeTools/Interfaces/IYamlMapping.cs
+++ EspHomeTools/Interfaces/IYamlMapping.cs
@@ -2,14 +2,4 @@
 
 namespace EspHomeTools.Interfaces;
 
-/// <summary>
-/// Defines a structure representing a YAML mapping, consisting of a collection of key-value pairs.
-/// The keys are represented as strings, while the values are YAML nodes implementing the IYamlNode interface.
-/// </summary>
-/// <remarks>
-/// This interface extends IDictionary to provide standard dictionary operations for managing
-/// the key-value pairs and integrates with YAML-specific functionalities through the IYamlStructure interface.
-/// It is intended to encapsulate YAML mapping behavior, supporting features such as metadata handling
-/// and YAML serialization or deserialization.
-/// </remarks>
 public interface IYamlMapping : IYamlStructure, IDictionary<string, IYamlNode> { }
