c70248480639c9d276b7e062770f7517285bfd78
Andreas <Andreas.Zwander@gmail.com>
2025-07-22 00:34:23 +0200

    Refactor `OtaBuilder` class: update to implement `IYamlBuilder` for consistency with other builder classes.

--- EspHomeTools/Builders/OtaBuilder.cs
+++ EspHomeTools/Builders/OtaBuilder.cs
@@ -5,7 +5,7 @@ using EspHomeTools.Interfaces;
 
 namespace EspHomeTools.Builders;
 
-public class OtaBuilder: IYamlBuilder<IYamlMapping>
+public class OtaBuilder: IYamlBuilder<IYamlMapping>, IYamlBuilder
 {
     private const string DefaultPlatform = "esphome";
 
