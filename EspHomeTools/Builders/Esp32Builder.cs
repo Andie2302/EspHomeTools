451741aa87284f64ab2402414a49326a07ca2642
Andreas <Andreas.Zwander@gmail.com>
2025-07-22 00:33:22 +0200

    Refactor `Esp32Builder` class: update to implement `IYamlBuilder` for consistency with other builder classes.

--- EspHomeTools/Builders/Esp32Builder.cs
+++ EspHomeTools/Builders/Esp32Builder.cs
@@ -5,7 +5,7 @@ using EspHomeTools.Interfaces;
 
 namespace EspHomeTools.Builders;
 
-public class Esp32Builder: IYamlBuilder<IYamlMapping>
+public class Esp32Builder: IYamlBuilder<IYamlMapping>, IYamlBuilder
 {
     private readonly YamlMapping _block = new();
 
