7982f3dba4b53c2dc754ef0830fe847825ef4d40
Andreas <Andreas.Zwander@gmail.com>
2025-07-21 20:05:41 +0200

    Update XML documentation and modify `ToYaml` method signature in `IYamlSerializable` interface.

--- EspHomeTools/Interfaces/IYamlSerializable.cs
+++ EspHomeTools/Interfaces/IYamlSerializable.cs
@@ -6,12 +6,11 @@
 /// </summary>
 public interface IYamlSerializable
 {
+    /// <summary>
     /// Converts the object to a YAML string representation.
-    /// <param name="indent">
-    /// The number of spaces to prepend to each line for indentation. Default is 0.
-    /// </param>
-    /// <returns>
-    /// A string representing the YAML formatted content of the object.
-    /// </returns>
-    string ToYaml(int indent = 0);
+    /// </summary>
+    /// <param name="name">The name (key) to be used for this node when it's part of a mapping. Can be null.</param>
+    /// <param name="indent">The number of spaces to prepend to each line for indentation. Default is 0.</param>
+    /// <returns>A string representing the YAML formatted content of the object.</returns>
+    string ToYaml(int indent, string? name);
 }
