eee606b46528a1dbfb1f57209d58022aa3c4b3d6
Andreas <Andreas.Zwander@gmail.com>
2025-07-22 00:44:44 +0200

    Refactor `YamlLambda` class: declare as `sealed`, fix null check in `AppendComments`, and update method signature for `ToYaml`.

--- EspHomeTools/Builders/YamlLambda.cs
+++ EspHomeTools/Builders/YamlLambda.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using System.Text;
+using EspHomeTools.Classes;
 using EspHomeTools.Classes.Scalars;
 
 namespace EspHomeTools.Builders;
 
-public class YamlLambda : YamlScalar<string>
+public sealed class YamlLambda : YamlScalar<string>
 {
     private const int CodeIndentationSpaces = 2;
     private readonly static string[] LineBreakSeparators = ["\r\n", "\r", "\n"];
 
     public YamlLambda(string value) => Value = value;
 
-    public override string ToYaml(int indent = 0, string? name = null)
+    public override string ToYaml(int indent, string? name)
     {
         var sb = new StringBuilder();
         var baseIndentation = new string(' ', indent);
@@ -23,8 +24,9 @@ public class YamlLambda : YamlScalar<string>
 
     private void AppendComments(StringBuilder sb, string baseIndentation)
     {
-        if (string.IsNullOrWhiteSpace(Comment)) return;
-        var commentLines = Comment.Split(LineBreakSeparators, StringSplitOptions.None);
+        if (Comment==null) return;
+        var temp = YamlTools.Normalize(Comment);
+        var commentLines = temp.Split(LineBreakSeparators, StringSplitOptions.None);
         foreach (var commentLine in commentLines)
         {
             sb.Append(baseIndentation).Append("# ").AppendLine(commentLine);
