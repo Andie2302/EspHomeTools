6cc852261b51806293ca528bb8f21126f1d73c36
Andreas <Andreas.Zwander@gmail.com>
2025-07-22 00:34:09 +0200

    Refactor `LightBuilder` class: update to implement `IYamlBuilder` for consistency with other builder classes.

--- EspHomeTools/Builders/LightBuilder.cs
+++ EspHomeTools/Builders/LightBuilder.cs
@@ -5,7 +5,7 @@ using EspHomeTools.Interfaces;
 
 namespace EspHomeTools.Builders;
 
-public class LightBuilder: IYamlBuilder<IYamlMapping>
+public class LightBuilder: IYamlBuilder<IYamlMapping>, IYamlBuilder
 {
     private const string PlatformKey = "platform";
 
