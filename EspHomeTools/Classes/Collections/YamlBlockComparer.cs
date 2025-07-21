d57f507e57987d26122a322aafdf7d9b9263a41d
Andreas <Andreas.Zwander@gmail.com>
2025-07-21 03:14:46 +0200

    Refactor `YamlBlockComparer` by introducing constants and a helper method to reduce redundancy, improve readability, and enhance maintainability.

--- EspHomeTools/Classes/Collections/YamlBlockComparer.cs
+++ EspHomeTools/Classes/Collections/YamlBlockComparer.cs
@@ -4,20 +4,24 @@ using EspHomeTools.Interfaces;
 
 namespace EspHomeTools.Classes.Collections;
 
+
 public class YamlBlockComparer : IComparer<KeyValuePair<string, IYamlNode>>
 {
+    private const int NotFoundPriority = int.MaxValue;
     private readonly List<string> _customOrder;
-    public YamlBlockComparer(List<string> customOrder)
-    {
-        _customOrder = customOrder;
-    }
+
+    public YamlBlockComparer(List<string> customOrder) => _customOrder = customOrder;
+
     public int Compare(KeyValuePair<string, IYamlNode> x, KeyValuePair<string, IYamlNode> y)
     {
-        var indexX = _customOrder.FindIndex(key => key.Equals(x.Key, StringComparison.OrdinalIgnoreCase));
-        var indexY = _customOrder.FindIndex(key => key.Equals(y.Key, StringComparison.OrdinalIgnoreCase));
-        if (indexX == -1) indexX = int.MaxValue;
-        if (indexY == -1) indexY = int.MaxValue;
-        return indexX == indexY ? string.Compare(x.Key, y.Key, StringComparison.Ordinal) : indexX.CompareTo(indexY);
+        var priorityX = GetPriorityIndex(x.Key);
+        var priorityY = GetPriorityIndex(y.Key);
+        return priorityX == priorityY ? string.Compare(x.Key, y.Key, StringComparison.Ordinal) : priorityX.CompareTo(priorityY);
     }
 
+    private int GetPriorityIndex(string key)
+    {
+        var index = _customOrder.FindIndex(orderKey => orderKey.Equals(key, StringComparison.OrdinalIgnoreCase));
+        return index == -1 ? NotFoundPriority : index;
+    }
 }
