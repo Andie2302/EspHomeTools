e7d794a15b5c60f71774e36ed2cdf4d34db42eb8
Andreas <Andreas.Zwander@gmail.com>
2025-07-22 22:06:49 +0200

    Refactor `YamlRenderManager` to simplify structure and enhance readability; introduce `YamlConstants` for shared YAML formatting definitions.

--- EspHomeTools/Classes/YamlRenderManager.cs
+++ EspHomeTools/Classes/YamlRenderManager.cs
@@ -1,73 +1,51 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 
 namespace EspHomeTools.Classes;
 
-public class YamlRenderManager
+public static class YamlConstants
 {
-    #region Variables
-
-    private readonly YamlRenderSettings _settings;
-    private readonly StringBuilder _stringBuilder = new();
-
-    #endregion
-
-
-    #region Constructors
-
-    public YamlRenderManager(YamlRenderSettings? settings = null) => _settings = settings ?? new YamlRenderSettings();
-    public YamlRenderManager(int spacesPerLevel, char indentationCharacter) => _settings = new YamlRenderSettings(spacesPerLevel, indentationCharacter);
-
-    #endregion
-
-
-    #region Public Append Methods
-
-    public void Append(string text) => _stringBuilder.Append(text);
-    public void AppendLine(string text) => _stringBuilder.AppendLine(text);
-    public void AppendLine() => _stringBuilder.AppendLine();
-    public void AppendIndentation(int indentationLevel) => _stringBuilder.Append(_settings.GetIndentationString(indentationLevel));
-
-    #endregion
-
-
-    #region Special Append Methods
-
     public const string ListItemPrefix = "- ";
     public const string EmptyList = "[]";
-    public const int ItemIndentationIncrease = 1;
-    public void AppendListItemPrefix() => Append(ListItemPrefix);
-    public void AppendEmptyList() => Append(EmptyList);
+    public const string CommentPrefix = "# ";
+    public const string EmptyLine = "\r\n";
+}
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
+[SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
+public class YamlRenderManager
+{
+    private readonly StringBuilder _stringBuilder = new();
+    private readonly YamlRenderSettings _settings;
 
-    public static void RenderItems(List<YamlElement> items, YamlRenderManager manager, int indentationLevel)
+    public YamlRenderManager(YamlRenderSettings? settings = null) => _settings = settings ?? new YamlRenderSettings();
+
+    public YamlRenderManager(int spacesPerLevel, char indentationCharacter) => _settings = new YamlRenderSettings(spacesPerLevel, indentationCharacter);
+
+    public void Append(string text, int indentationLevel) => AppendIndentedText(indentationLevel, text);
+
+    public void AppendEmptyLine() => _stringBuilder.Append(YamlConstants.EmptyLine);
+
+    public void AppendLine(string text, int indentationLevel) => AppendIndentedText(indentationLevel, text, YamlConstants.EmptyLine);
+
+    public void AppendComment(string line, int indentationLevel) => AppendIndentedText(indentationLevel, YamlConstants.CommentPrefix, line, YamlConstants.EmptyLine);
+
+    public void AppendComment(IEnumerable<string> lines, int indentationLevel) => AppendMultipleComments(lines, indentationLevel);
+
+    private void AppendMultipleComments(IEnumerable<string> lines, int indentationLevel)
     {
-        if (items.Count != 0)
+        foreach (var line in lines)
         {
-            for (var index = 0; index < items.Count; index++)
-            {
-                if (index > 0)
-                {
-                    manager.AppendLine();
-                }
-
-                manager.AppendIndentation(indentationLevel);
-                manager.AppendListItemPrefix();
-                items[index].Render(manager, indentationLevel + ItemIndentationIncrease);
-            }
-        }
-        else
-        {
-            manager.AppendEmptyList();
+            AppendComment(line, indentationLevel);
         }
     }
 
-    #endregion
+    private void AppendIndentedText(int indentationLevel, params string[] textParts)
+    {
+        _stringBuilder.Append(_settings.GetIndentationString(indentationLevel));
+        foreach (var part in textParts)
+            _stringBuilder.Append(part);
+    }
 
-
-    #region Public Control Methods
-
-    public void Clear() => _stringBuilder.Clear();
-    public string GetResult() => _stringBuilder.ToString();
-
-    #endregion
+    public string Result => _stringBuilder.ToString();
 }
