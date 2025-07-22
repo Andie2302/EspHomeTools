b117f0401d9c1df838106fe5f00aa0aa920e7856
Andreas <Andreas.Zwander@gmail.com>
2025-07-22 22:09:25 +0200

    Add `YamlConstants` class for shared YAML-related constants in `EspHomeTools`.

---
+++ EspHomeTools/Classes/YamlConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace EspHomeTools.Classes;
+
+public static class YamlConstants
+{
+    public const string ListItemPrefix = "- ";
+    public const string EmptyList = "[]";
+    public const string CommentPrefix = "# ";
+    public const string EmptyLine = "\r\n";
+}
