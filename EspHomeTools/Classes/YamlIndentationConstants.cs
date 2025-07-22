d69c3a9bc2ebebeae9a86acd0d967267e04adb1f
Andreas <Andreas.Zwander@gmail.com>
2025-07-22 22:50:50 +0200

    Fix syntax error in `YamlIndentationConstants` array initialization.

--- EspHomeTools/Classes/YamlIndentationConstants.cs
+++ EspHomeTools/Classes/YamlIndentationConstants.cs
@@ -5,5 +5,5 @@ public static class YamlIndentationConstants
     public const int MinSpacesPerLevel = 0;
     public const int MaxRecommendedSpacesPerLevel = 10;
     public const int MinIndentationLevel = 0;
-    public readonly static char[] AllowedIndentationCharacters = { ' ', '\t' };
+    public readonly static char[] AllowedIndentationCharacters = [' ', '\t'];
 }
