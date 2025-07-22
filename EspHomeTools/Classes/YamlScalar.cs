6bb0ddd66df859f562af278ecbbffee41976eadc
Andreas <Andreas.Zwander@gmail.com>
2025-07-22 22:56:15 +0200

    Introduce `YamlScalar` class with unimplemented `Render` method in `EspHomeTools`.

---
+++ EspHomeTools/Classes/YamlScalar.cs
@@ -0,0 +1,9 @@
+﻿namespace EspHomeTools.Classes;
+
+public class YamlScalar : IYamlScalar
+{
+    public void Render(YamlRenderManager yamlRenderManager, int indentationLevel)
+    {
+        throw new System.NotImplementedException();
+    }
+}
