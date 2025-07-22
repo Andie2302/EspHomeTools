67127602378017f4002740046ee2b38152ce8d80
Andreas <Andreas.Zwander@gmail.com>
2025-07-22 22:47:58 +0200

    Extract `IYamlList` interface from `YamlList` and refactor class definition for clarity and reusability.

--- EspHomeTools/Classes/YamlList.cs
+++ EspHomeTools/Classes/YamlList.cs
@@ -3,19 +3,26 @@ using System.Collections.Generic;
 
 namespace EspHomeTools.Classes;
 
-public class YamlList : IYamlObject, IEnumerable<IYamlScalar>
+public class YamlList : IYamlList
 {
     private readonly List<IYamlScalar> _items = [];
+
     public YamlList(params IYamlScalar[] items) => _items.AddRange(items);
+
     public void Add(IYamlScalar item) => _items.Add(item);
+
     public int Count => _items.Count;
 
     public bool HasItems => _items.Count != 0;
 
     public IEnumerator<IYamlScalar> GetEnumerator() => _items.GetEnumerator();
+
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-    public void Render(YamlRenderManager yamlRenderManager, int indentationLevel)
-    {
-        throw new System.NotImplementedException();
-    }
+    public void Render(YamlRenderManager yamlRenderManager, int indentationLevel) => throw new System.NotImplementedException();
+}
+public interface IYamlList : IYamlObject, IEnumerable<IYamlScalar>
+{
+    int Count { get; }
+    bool HasItems { get; }
+    void Add(IYamlScalar item);
 }
