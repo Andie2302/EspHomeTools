8895b3c3899b810b366f38ece2119f7ab456f3a3
Andreas <Andreas.Zwander@gmail.com>
2025-07-22 22:22:04 +0200

    Add `HasItems` property to `YamlList` and tidy up formatting

--- EspHomeTools/Classes/YamlList.cs
+++ EspHomeTools/Classes/YamlList.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
 namespace EspHomeTools.Classes;
+
 public class YamlList : IYamlObject, IEnumerable<IYamlScalar>
 {
     private readonly List<IYamlScalar> _items = [];
     public YamlList(params IYamlScalar[] items) => _items.AddRange(items);
     public void Add(IYamlScalar item) => _items.Add(item);
     public int Count => _items.Count;
+
     public bool HasItems => _items.Count != 0;
-    //public void Render(YamlRenderManager manager, int indentationLevel) => //YamlRenderManager.RenderItems(_items, manager, indentationLevel);
+
     public IEnumerator<IYamlScalar> GetEnumerator() => _items.GetEnumerator();
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     public void Render(YamlRenderManager yamlRenderManager, int indentationLevel)
