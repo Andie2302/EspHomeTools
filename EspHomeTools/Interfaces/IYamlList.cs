ff74c316965ed831603809d934a219e4c93130be
Andreas <Andreas.Zwander@gmail.com>
2025-07-22 23:00:21 +0200

    Add `IYamlList` interface defining YAML list structure and operations in `EspHomeTools`

---
+++ EspHomeTools/Interfaces/IYamlList.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using EspHomeTools.Interfaces;
+
+namespace EspHomeTools.Classes;
+
+public interface IYamlList : IYamlObject, IEnumerable<IYamlScalar>
+{
+    int Count { get; }
+    bool HasItems { get; }
+    void Add(IYamlScalar item);
+}
