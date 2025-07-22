b5728888e0d1c9cedcb88961001df6502abf598e
Andreas <Andreas.Zwander@gmail.com>
2025-07-22 22:06:40 +0200

    Refactor `YamlList` to use `IYamlScalar` interface and adjust member types; introduce stub `YamlRenderManagerALT` class.

--- EspHomeTools/Classes/YamlList.cs
+++ EspHomeTools/Classes/YamlList.cs
@@ -1,17 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 namespace EspHomeTools.Classes;
-public class YamlList : YamlElement, IEnumerable<YamlElement>
+public class YamlList : IYamlObject, IEnumerable<IYamlScalar>
 {
-    private readonly List<YamlElement> _items = [];
-    public YamlList(params YamlElement[] items) => _items.AddRange(items);
-    public void Add(YamlElement item) => _items.Add(item);
+    private readonly List<IYamlScalar> _items = [];
+    public YamlList(params IYamlScalar[] items) => _items.AddRange(items);
+    public void Add(IYamlScalar item) => _items.Add(item);
     public int Count => _items.Count;
     public bool HasItems => _items.Count != 0;
-    public override void Render(YamlRenderManager manager, int indentationLevel)
-    {
-        YamlRenderManager.RenderItems(_items, manager, indentationLevel);
-    }
-    public IEnumerator<YamlElement> GetEnumerator() => _items.GetEnumerator();
+    //public void Render(YamlRenderManager manager, int indentationLevel) => //YamlRenderManager.RenderItems(_items, manager, indentationLevel);
+    public IEnumerator<IYamlScalar> GetEnumerator() => _items.GetEnumerator();
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
+
+public interface IYamlScalar : IYamlObject
+{
+
 }
---
+++ EspHomeTools/Classes/YamlRenderManagerALT.cs
@@ -0,0 +1,28 @@
+﻿namespace EspHomeTools.Classes;
+
+public class YamlRenderManagerALT
+{
+    /*
+    public static void RenderItems(List<YamlObject> items, YamlRenderManager manager, int indentationLevel)
+    {
+        if (items.Count != 0)
+        {
+            for (var index = 0; index < items.Count; index++)
+            {
+                if (index > 0)
+                {
+                    manager.AppendLine();
+                }
+
+                manager.AppendIndentation(indentationLevel);
+                manager.AppendListItemPrefix();
+                items[index].Render(manager, indentationLevel + ItemIndentationIncrease);
+            }
+        }
+        else
+        {
+            manager.AppendEmptyList();
+        }
+    }
+    //*/
+}
