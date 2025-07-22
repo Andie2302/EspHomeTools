49bc0d847c68dfa46c5961c3f0020cc7696a28b4
Andreas <Andreas.Zwander@gmail.com>
2025-07-22 22:40:10 +0200

    Add `YamlIndentationConstants` class for YAML indentation-related constants in `EspHomeTools`.

---
+++ EspHomeTools/Classes/YamlIndentationConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace EspHomeTools.Classes;
+
+public static class YamlIndentationConstants
+{
+    public const int MinSpacesPerLevel = 0;
+    public const int MaxRecommendedSpacesPerLevel = 10;
+    public const int MinIndentationLevel = 0;
+    public readonly static char[] AllowedIndentationCharacters = { ' ', '\t' };
+}
