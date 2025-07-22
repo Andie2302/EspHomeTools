7eec9890bc952164f1818bbab20f3f6b6194ad99
Andreas <Andreas.Zwander@gmail.com>
2025-07-22 20:04:56 +0200

    Remove unused `AppendIndentationString` method from `YamlRenderSettings` to simplify the class.

--- EspHomeTools/Classes/YamlRenderSettings.cs
+++ EspHomeTools/Classes/YamlRenderSettings.cs
@@ -1,19 +1,16 @@
-﻿using System.Text;
-
+﻿
+using System.Text;
 namespace EspHomeTools.Classes;
-
 public class YamlRenderSettings
 {
     private const char DefaultIndentationCharacter = ' ';
     private const int DefaultSpacesPerLevel = 2;
     public int SpacesPerLevel { get; }
     public char IndentationCharacter { get; }
-
     public YamlRenderSettings(int spacesPerLevel = DefaultSpacesPerLevel, char indentationCharacter = DefaultIndentationCharacter)
     {
         SpacesPerLevel = spacesPerLevel;
         IndentationCharacter = indentationCharacter;
     }
     public string GetIndentationString(int indentationLevel) => new(IndentationCharacter, indentationLevel * SpacesPerLevel);
-    public void AppendIndentationString(StringBuilder stringBuilder, int indentationLevel) => stringBuilder.Append(GetIndentationString(indentationLevel));
 }
