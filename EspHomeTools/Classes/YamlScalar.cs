f661db4fc3455d2a3f961ef598c2d8d4c157d1c4
Andreas <Andreas.Zwander@gmail.com>
2025-07-22 23:00:43 +0200

    Implement `Render` method in `YamlScalar` with basic logging.

--- EspHomeTools/Classes/YamlScalar.cs
+++ EspHomeTools/Classes/YamlScalar.cs
@@ -1,9 +1,12 @@
-﻿namespace EspHomeTools.Classes;
+﻿using System;
+using EspHomeTools.Interfaces;
+
+namespace EspHomeTools.Classes;
 
 public class YamlScalar : IYamlScalar
 {
     public void Render(YamlRenderManager yamlRenderManager, int indentationLevel)
     {
-        throw new System.NotImplementedException();
+        Console.WriteLine("Hallo von Render in YamlScalar!");
     }
 }
