c0e8e354eaccc7384d5b836095eae72fa6de680e
Andreas <Andreas.Zwander@gmail.com>
2025-07-22 20:03:55 +0200

    Refactor `YamlList` class to streamline rendering logic and improve encapsulation.

--- EspHomeTools/Classes/YamlList.cs
+++ EspHomeTools/Classes/YamlList.cs
@@ -1,42 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 namespace EspHomeTools.Classes;
-
 public class YamlList : YamlElement, IEnumerable<YamlElement>
 {
     private readonly List<YamlElement> _items = [];
-
-    public YamlList(params YamlElement[] items)
-    {
-        _items.AddRange(items);
-    }
-
+    public YamlList(params YamlElement[] items) => _items.AddRange(items);
     public void Add(YamlElement item) => _items.Add(item);
-
+    public int Count => _items.Count;
+    public bool HasItems => _items.Count != 0;
     public override void Render(YamlRenderManager manager, int indentationLevel)
     {
-        if (!_items.Any())
-        {
-            manager.Append("[]");
-            return;
-        }
-
-        for (var i = 0; i < _items.Count; i++)
-        {
-            var item = _items[i];
-            manager.AppendIndentation(indentationLevel);
-            manager.Append("- ");
-            item.Render(manager, indentationLevel + 1);
-            if (i < _items.Count - 1)
-            {
-                manager.AppendLine();
-            }
-        }
+        YamlRenderManager.RenderItems(_items, manager, indentationLevel);
     }
-
     public IEnumerator<YamlElement> GetEnumerator() => _items.GetEnumerator();
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }
