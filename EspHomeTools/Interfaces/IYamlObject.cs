6e5f7f9bf753191df4fb26fc099c3a195dc21812
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 04:01:32 +0200

    Reintroduce `IYamlValue<T>` and `IYamlScalar<T>` interfaces in `EspHomeTools.Interfaces`.

--- EspHomeTools/Interfaces/IYamlObject.cs
+++ EspHomeTools/Interfaces/IYamlObject.cs
@@ -3,3 +3,11 @@
 namespace EspHomeTools.Interfaces;
 
 public interface IYamlObject : IYamlRenderable { }
+
+public interface IYamlValue<T> : IYamlValue
+{
+    T? Value { get; set; }
+    bool HasValue { get; }
+}
+
+public interface IYamlScalar<T> : IYamlValue<T>, IYamlScalar { }
