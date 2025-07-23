7fc640ea0e1178a5ab71df971e90665b50a2482b
Andreas <Andreas.Zwander@gmail.com>
2025-07-24 01:20:35 +0200

    Add `IYamlMapping` interface to define a contract for YAML mappings.

---
+++ EspHomeTools/Classes/Structures/IYamlMapping.cs
@@ -0,0 +1,10 @@
+﻿using EspHomeTools.Interfaces;
+
+namespace EspHomeTools.Classes.Structures;
+
+public interface IYamlMapping : IYamlStructure
+{
+    IYamlNode this[string key] { get; set; }
+    bool TryGetValue(string key, out IYamlNode? value);
+    bool ContainsKey(string key);
+}
