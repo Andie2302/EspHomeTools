4300ced89b55856b3826dc0863afbb9f9fe1c2ad
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 03:23:28 +0200

    Introduce `IYamlRenderable` interface in `EspHomeTools.Interfaces.Render`.

---
+++ EspHomeTools/Interfaces/Render/IYamlRenderable.cs
@@ -0,0 +1,14 @@
+﻿namespace EspHomeTools.Interfaces.Render;
+
+/// <summary>
+/// Defines a contract for objects that can render their state into YAML format.
+/// </summary>
+public interface IYamlRenderable
+{
+    /// <summary>
+    /// Renders the YAML representation of an object using the specified render manager and indentation level.
+    /// </summary>
+    /// <param name="yamlRenderManager">The render manager used to generate YAML content.</param>
+    /// <param name="indentationLevel">The level of indentation to apply during rendering.</param>
+    void Render(IYamlRenderManager yamlRenderManager, int indentationLevel);
+}
