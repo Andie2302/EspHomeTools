bf6078ebbbf8c3b5b899cea3e9d1b80bb54e46cb
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 14:21:02 +0200

    Simplify `IYamlObject` interface by removing inheritance from `IYamlRenderable` in `EspHomeTools.Interfaces`.

--- EspHomeTools/Interfaces/IYamlObject.cs
+++ EspHomeTools/Interfaces/IYamlObject.cs
@@ -1,10 +1,3 @@
 ﻿namespace EspHomeTools.Interfaces;
 
-/// <summary>
-/// Represents a base interface for objects that can be serialized into YAML format.
-/// </summary>
-/// <remarks>
-/// This interface extends the capabilities of <see cref="IYamlRenderable"/>
-/// by serving as the foundation for YAML object representation.
-/// </remarks>
-public interface IYamlObject : IYamlRenderable;
+public interface IYamlObject;
