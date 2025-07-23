612dd968998d57b031087d72898ce368d4209b24
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 13:53:06 +0200

    Remove `IYamlValue` and `IYamlValue<T>` interfaces from `EspHomeTools.Interfaces.Base`.

--- EspHomeTools/Interfaces/Base/IYamlValue.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EspHomeTools.Interfaces.Base;
-
-public interface IYamlValue { }
-
-public interface IYamlValue<T> : IYamlValue
-{
-    T? Value { get; set; }
-    bool HasValue { get; }
-}
