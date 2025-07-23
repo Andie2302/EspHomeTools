91619a9613ed30dabef7f25727fa8318fe0fcdb3
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 14:30:58 +0200

    Add `IYamlScalar` interface to `EspHomeTools.Scratch.Interfaces.Scalars`.

---
+++ EspHomeTools/Interfaces/IYamlScalar.cs
@@ -0,0 +1,7 @@
+﻿using EspHomeTools.Interfaces;
+
+namespace EspHomeTools.Scratch.Interfaces.Scalars;
+
+public interface IYamlScalar;
+
+public interface IYamlScalar<T> : IYamlScalar, IYamlValue<T>;
