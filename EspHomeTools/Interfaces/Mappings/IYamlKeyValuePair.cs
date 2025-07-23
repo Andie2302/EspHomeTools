406939b925e58f32899db0f6c2f8957cf3a3d875
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 13:48:10 +0200

    Add `IYamlKeyValuePair` interface to `EspHomeTools.Interfaces.Mappings`.

---
+++ EspHomeTools/Interfaces/Mappings/IYamlKeyValuePair.cs
@@ -0,0 +1,6 @@
+﻿using System.Collections.Generic;
+using EspHomeTools.Interfaces.Base;
+
+namespace EspHomeTools.Interfaces.Mappings;
+
+public interface IYamlKeyValuePair<T> : IYamlKey, IYamlValue<T> { }
