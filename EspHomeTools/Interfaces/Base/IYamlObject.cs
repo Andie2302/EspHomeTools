6a53726a18a4435a119bec93272ae9bb2f0b2f63
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 13:47:58 +0200

    Remove unused `IYamlValue` and `IYamlScalar` interfaces from `IYamlObject`.

--- EspHomeTools/Interfaces/Base/IYamlObject.cs
+++ EspHomeTools/Interfaces/Base/IYamlObject.cs
@@ -1,14 +1,5 @@
 ﻿using EspHomeTools.Interfaces.RenderManagers;
-using EspHomeTools.Interfaces.Scalars;
 
 namespace EspHomeTools.Interfaces.Base;
 
 public interface IYamlObject : IYamlRenderable { }
-
-public interface IYamlValue<T> : IYamlValue
-{
-    T? Value { get; set; }
-    bool HasValue { get; }
-}
-
-public interface IYamlScalar<T> : IYamlValue<T>, IYamlScalar { }
