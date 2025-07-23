2ea18091eddb61ac8faa5ec91585f39cefd2dc12
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 04:01:25 +0200

    Add `IYamlList` interface in `EspHomeTools.DEMO`.

---
+++ EspHomeTools/DEMO/IYamlList.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using EspHomeTools.Interfaces;
+
+/// <summary>
+/// Definiert eine Liste von YAML-Skalarwerten.
+/// </summary>
+public interface IYamlList : IYamlObject, IEnumerable<IYamlScalar>
+{
+    void Add(IYamlScalar item);
+    int Count { get; }
+}
