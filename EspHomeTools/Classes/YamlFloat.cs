9393b54359328fb153fd1ba573213cb1bcd0fb25
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 14:45:52 +0200

    Add `YamlFloat`, `YamlInteger`, `YamlString`, and `YamlNull` classes implementing respective interfaces

---
+++ EspHomeTools/Classes/YamlFloat.cs
@@ -0,0 +1,43 @@
+﻿using EspHomeTools.Interfaces;
+using EspHomeTools.Scratch.Interfaces.RenderManagers;
+
+namespace EspHomeTools.Classes;
+
+public class YamlFloat : IYamlFloat
+{
+    public void Render(IYamlRenderManager yamlRenderManager, int indentationLevel)
+    {
+        throw new System.NotImplementedException();
+    }
+    public float Value { get; set; }
+}
+
+public class YamlInteger : IYamlInteger
+{
+    public void Render(IYamlRenderManager yamlRenderManager, int indentationLevel)
+    {
+        throw new System.NotImplementedException();
+    }
+    public int Value { get; set; }
+}
+
+public class YamlString : IYamlString
+{
+    public void Render(IYamlRenderManager yamlRenderManager, int indentationLevel)
+    {
+        throw new System.NotImplementedException();
+    }
+    public string? Value { get; set; }
+    public bool IsValueNull => Value == null;
+    public bool HasValue => string.IsNullOrWhiteSpace(Value) == false;
+}
+public class YamlNull : IYamlNull, IYamlNullable
+{
+    public void Render(IYamlRenderManager yamlRenderManager, int indentationLevel)
+    {
+        throw new System.NotImplementedException();
+    }
+    public string? Value { get; set; } = null;
+    public bool IsValueNull => true;
+}
+
