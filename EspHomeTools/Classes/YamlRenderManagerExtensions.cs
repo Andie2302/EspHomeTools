3ca3926157c4d3b92effb577a0af9db1f95b6b6b
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 22:49:36 +0200

    Add `AppendScalar` method to `YamlRenderManagerExtensions` for appending `YamlBoolean` values.

--- EspHomeTools/Classes/YamlRenderManagerExtensions.cs
+++ EspHomeTools/Classes/YamlRenderManagerExtensions.cs
@@ -1,19 +1,21 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using EspHomeTools.Interfaces;
 
 namespace EspHomeTools.Classes;
 
 [SuppressMessage("ReSharper", "UnusedType.Global")]
 public static class YamlRenderManagerExtensions
 {
-   public static void AppendComment(this YamlRenderManager manager, string comment, int indentationLevel,bool linebreak)
-   {
-      if (linebreak)
-      {
-         manager.AppendLine($"# {comment}", indentationLevel);
-      }
-      else
-      {
-         manager.Append($"# {comment}", indentationLevel);
-      }
-   }
+    public static void AppendComment(this YamlRenderManager manager, string comment, int indentationLevel, bool linebreak)
+    {
+        if (linebreak)
+        {
+            manager.AppendLine($"# {comment}", indentationLevel);
+        }
+        else
+        {
+            manager.Append($"# {comment}", indentationLevel);
+        }
+    }
+    public static void AppendScalar(this IYamlRenderManager manager, YamlBoolean yamlBoolean, int indentationLevel) => manager.Append(yamlBoolean.ToString()?.ToLower() ?? string.Empty, indentationLevel);
 }
