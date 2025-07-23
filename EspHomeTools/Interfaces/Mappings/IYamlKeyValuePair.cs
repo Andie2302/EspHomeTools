692b28d0668a5825c769d3b6f18a2bd2b55d4a1e
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 13:52:12 +0200

    Remove `IYamlKeyValuePair` interface from `EspHomeTools.Interfaces.Mappings`.

--- EspHomeTools/Interfaces/Mappings/IYamlKeyValuePair.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Collections.Generic;
-using EspHomeTools.Interfaces.Base;
-
-namespace EspHomeTools.Interfaces.Mappings;
-
-public interface IYamlKeyValuePair<T> : IYamlKey, IYamlValue<T> { }
