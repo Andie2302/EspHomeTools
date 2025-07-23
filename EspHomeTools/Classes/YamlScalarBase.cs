905ad9c724e8328be802062b99d99eb7147b9d42
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 23:30:35 +0200

    Add `YamlScalarBase<T>` abstract class to streamline scalar type implementations and reuse rendering logic.

---
+++ EspHomeTools/Classes/YamlScalarBase.cs
@@ -0,0 +1,10 @@
+﻿using EspHomeTools.Interfaces;
+
+namespace EspHomeTools.Classes;
+
+public abstract class YamlScalarBase<T> : IYamlScalar<T>
+{
+    public virtual void Render(IYamlRenderManager yamlRenderManager, int indentationLevel) => yamlRenderManager.AppendScalar(this, indentationLevel);
+
+    public virtual T Value { get; set; }
+}
