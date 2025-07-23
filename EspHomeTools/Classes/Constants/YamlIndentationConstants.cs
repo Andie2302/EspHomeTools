c531eec91ed429e8ed9cede956dd7fcb4925980b
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 03:09:24 +0200

    Remove `YamlIndentationConstants` class as it is unused and redundant.

--- EspHomeTools/Classes/Constants/YamlIndentationConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EspHomeTools.Classes.Constants;
-
-public static class YamlIndentationConstants
-{
-    public const int MinSpacesPerLevel = 0;
-    public const int MaxRecommendedSpacesPerLevel = 10;
-    public const int MinIndentationLevel = 0;
-    public readonly static char[] AllowedIndentationCharacters = [' ', '\t'];
-}
