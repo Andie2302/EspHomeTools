255f4eb798b8d598086af9cbaee7ff20a741bd7f
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 22:12:45 +0200

    Implement `Render` method in `YamlInteger` to append value using `IYamlRenderManager`.

--- EspHomeTools/Classes/YamlInteger.cs
+++ EspHomeTools/Classes/YamlInteger.cs
@@ -1,4 +1,6 @@
-﻿using EspHomeTools.Interfaces;
+﻿// using EspHomeTools.Interfaces;
+
+using EspHomeTools.Interfaces;
 
 namespace EspHomeTools.Classes;
 
@@ -6,7 +8,7 @@ public class YamlInteger : IYamlInteger
 {
     public void Render(IYamlRenderManager yamlRenderManager, int indentationLevel)
     {
-        throw new System.NotImplementedException();
+        yamlRenderManager.Append(Value.ToString(), indentationLevel);
     }
     public int Value { get; set; }
 }
