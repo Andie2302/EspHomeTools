4d585221aae59d307738f5d5207f88137e092a46
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 14:45:39 +0200

    Add `YamlBoolean` class implementing `IYamlBoolean`

---
+++ EspHomeTools/Classes/YamlBoolean.cs
@@ -0,0 +1,13 @@
+﻿using EspHomeTools.Interfaces;
+using EspHomeTools.Scratch.Interfaces.RenderManagers;
+
+namespace EspHomeTools.Classes;
+
+public class YamlBoolean : IYamlBoolean
+{
+    public void Render(IYamlRenderManager yamlRenderManager, int indentationLevel)
+    {
+        throw new System.NotImplementedException();
+    }
+    public bool Value { get; set; }
+}
