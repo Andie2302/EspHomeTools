d310d6f24a661a1cc52e5a72d2d1a0a4ae1726bb
Andreas <Andreas.Zwander@gmail.com>
2025-07-24 01:20:26 +0200

    Add `IYamlStructure` interface to define a common contract for YAML structures.

---
+++ EspHomeTools/Classes/Structures/IYamlStructure.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using EspHomeTools.Interfaces;
+
+namespace EspHomeTools.Classes.Structures;
+
+public interface IYamlStructure : IYamlNode
+{
+    IEnumerable<IYamlNode> Children { get; }
+    void Add(IYamlNode node);
+    void Clear();
+}
