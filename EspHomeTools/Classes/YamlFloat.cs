32db2b14a7ac37826457bcbfe69c91f6379eba33
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 23:11:06 +0200

    Refactor `Render` method in `YamlFloat` to use `AppendScalar` for improved reuse and simplicity.

--- EspHomeTools/Classes/YamlFloat.cs
+++ EspHomeTools/Classes/YamlFloat.cs
@@ -5,9 +5,6 @@ namespace EspHomeTools.Classes;
 
 public class YamlFloat : IYamlFloat
 {
-    public void Render(IYamlRenderManager yamlRenderManager, int indentationLevel)
-    {
-        yamlRenderManager.Append(Value.ToString(CultureInfo.InvariantCulture), indentationLevel);
-    }
+    public void Render(IYamlRenderManager yamlRenderManager, int indentationLevel) => yamlRenderManager.AppendScalar(this,indentationLevel);
     public float Value { get; set; }
 }
