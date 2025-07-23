a8a382e8d99a27ac18de968a156a900556d52c59
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 15:52:10 +0200

    Swap parameter order for `Append` and `AppendLine` methods in `IYamlRenderManager`.

--- EspHomeTools/Interfaces/IYamlRenderManager.cs
+++ EspHomeTools/Interfaces/IYamlRenderManager.cs
@@ -6,10 +6,10 @@
 public interface IYamlRenderManager
 {
     /// Appends the specified text to the internal string buffer at a given indentation level.
-    /// <param name="text">The text to append.</param>
     /// <param name="indentationLevel">The level of indentation to apply.
-    /// Values beyond recommended bounds are clamped internally.</param>
-    void Append(string text, int indentationLevel);
+    ///     Values beyond recommended bounds are clamped internally.</param>
+    /// <param name="text">The text to append.</param>
+    void Append(int indentationLevel, string text);
     /// <summary>
     /// Appends a newline character to the internal string builder without any additional text or indentation.
     /// </summary>
@@ -18,12 +18,12 @@ public interface IYamlRenderManager
     /// Appends the specified text followed by a newline to the internal string builder,
     /// using the specified indentation level to determine the number of leading spaces.
     /// </summary>
-    /// <param name="text">The text to append.</param>
     /// <param name="indentationLevel">
-    /// The level of indentation to apply. The level determines the amount of space
-    /// added before the text. It will be clamped within a valid range.
+    ///     The level of indentation to apply. The level determines the amount of space
+    ///     added before the text. It will be clamped within a valid range.
     /// </param>
-    void AppendLine(string text, int indentationLevel);
+    /// <param name="text">The text to append.</param>
+    void AppendLine(int indentationLevel, string text);
     /// Retrieves the current-accumulated result from the internal StringBuilder used for rendering a YAML structure.
     /// <returns>
     /// A string representing the full YAML content accumulated by the YamlRenderManager.
