fd3f21d1847f0c1654a5b147946a996e5e6325eb
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 15:52:26 +0200

    Swap parameter order for `ExAppendKeyValueLine`, `ExAppendCommentLine`, and `XXXAppendLineXXX` methods in `YamlRenderManagerExtensions`.

--- EspHomeTools/Classes/YamlRenderManagerExtensions.cs
+++ EspHomeTools/Classes/YamlRenderManagerExtensions.cs
@@ -6,17 +6,17 @@ namespace EspHomeTools.Classes;
 [SuppressMessage("ReSharper", "UnusedType.Global")]
 public static class YamlRenderManagerExtensions
 {
-    public static void XXXAppendLineXXX(this IYamlRenderManager manager, string prefix, string v) { }
+    public static void XXXAppendLineXXX(this IYamlRenderManager manager, int indentationLevel, string prefix, string v) { }
 
-    public static void ExAppendKeyValueLine(this IYamlRenderManager manager, string key, string value, int indentationLevel)
+    public static void ExAppendKeyValueLine(this IYamlRenderManager manager, int indentationLevel, string key, string value)
     {
-        manager.Append($"{key}: ", indentationLevel);
-        manager.Append(value, 0);
+        manager.Append(indentationLevel, $"{key}: ");
+        manager.Append(0, value);
         manager.AppendLine();
     }
 
-    public static void ExAppendCommentLine(this IYamlRenderManager manager, string comment, int indentationLevel)
+    public static void ExAppendCommentLine(this IYamlRenderManager manager, int indentationLevel, string comment)
     {
-        manager.AppendLine($"# {comment}", indentationLevel);
+        manager.AppendLine(indentationLevel, $"# {comment}");
     }
 }
