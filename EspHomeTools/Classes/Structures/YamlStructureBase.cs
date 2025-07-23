3541539371184285b3cb32bae277d7d013142ad3
Andreas <Andreas.Zwander@gmail.com>
2025-07-24 01:20:02 +0200

    Add abstract `YamlStructureBase` class to define a common base for YAML structures.

---
+++ EspHomeTools/Classes/Structures/YamlStructureBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using EspHomeTools.Interfaces;
+
+namespace EspHomeTools.Classes.Structures;
+
+public abstract class YamlStructureBase : IYamlStructure
+{
+    public abstract IEnumerable<IYamlNode> Children { get; }
+    public abstract void Add(IYamlNode node);
+    public abstract void Clear();
+
+    public void Render(IYamlRenderManager yamlRenderManager, int indentationLevel)
+    {
+        if (IsEmpty())
+        {
+            RenderEmpty(yamlRenderManager, indentationLevel);
+            return;
+        }
+
+        RenderEntries(yamlRenderManager, indentationLevel);
+    }
+
+    protected abstract bool IsEmpty();
+    protected abstract void RenderEmpty(IYamlRenderManager yamlRenderManager, int indentationLevel);
+    protected abstract void RenderEntries(IYamlRenderManager yamlRenderManager, int indentationLevel);
+}
