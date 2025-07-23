504cc6b6a17a17e5b90ff4aff6341f1eee0d1538
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 14:46:04 +0200

    Extend `IYamlString` to implement `IYamlNullable`.

--- EspHomeTools/Interfaces/IYamlString.cs
+++ EspHomeTools/Interfaces/IYamlString.cs
@@ -1,3 +1,5 @@
-﻿namespace EspHomeTools.Interfaces;
+﻿using EspHomeTools.Classes;
 
-public interface IYamlString : IYamlScalar<string>;
+namespace EspHomeTools.Interfaces;
+
+public interface IYamlString : IYamlScalar<string>, IYamlNullable;
