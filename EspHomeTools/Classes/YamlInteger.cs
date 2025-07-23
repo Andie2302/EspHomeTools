4f9be3b32497d474c9a63a0fa42492d80adcea07
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 23:25:56 +0200

    Refactor `YamlInteger` to inherit from `YamlScalarBase<int>` for improved simplicity and reuse.

--- EspHomeTools/Classes/YamlInteger.cs
+++ EspHomeTools/Classes/YamlInteger.cs
@@ -2,8 +2,4 @@
 
 namespace EspHomeTools.Classes;
 
-public class YamlInteger : IYamlInteger
-{
-    public void Render(IYamlRenderManager yamlRenderManager, int indentationLevel) => yamlRenderManager.AppendScalar(this,indentationLevel);
-    public int Value { get; set; }
-}
+public class YamlInteger : YamlScalarBase<int>, IYamlInteger { }
