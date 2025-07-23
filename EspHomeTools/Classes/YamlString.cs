aa7221ee2980cbd0668fc93a928d9ce6c377e872
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 23:11:25 +0200

    Refactor `Render` method in `YamlString` to simplify and reuse `AppendScalar` for rendering.

--- EspHomeTools/Classes/YamlString.cs
+++ EspHomeTools/Classes/YamlString.cs
@@ -4,17 +4,8 @@ namespace EspHomeTools.Classes;
 
 public class YamlString : IYamlString
 {
-    private const string NullValue = "null";
-    private const string BackslashEscape = @"\\";
-    private const string QuoteEscape = "\\\"";
     public string? Value { get; set; }
     public bool IsValueNull => Value == null;
     public bool HasValue => string.IsNullOrWhiteSpace(Value) == false;
-    public void Render(IYamlRenderManager yamlRenderManager, int indentationLevel)
-    {
-        var renderValue = GetRenderValue();
-        yamlRenderManager.Append(renderValue, indentationLevel);
-    }
-    private string GetRenderValue() => IsValueNull ? NullValue : $"\"{EscapeString(Value ?? string.Empty)}\"";
-    private static string EscapeString(string input) => input.Replace("\\", BackslashEscape).Replace("\"", QuoteEscape);
+    public void Render(IYamlRenderManager yamlRenderManager, int indentationLevel) => yamlRenderManager.AppendScalar(this, indentationLevel);
 }
