0e20411a1707ac185ce63442c4477fdcb1ada6e1
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 14:50:04 +0200

    Add `YamlString` class implementing `IYamlString`

---
+++ EspHomeTools/Classes/YamlString.cs
@@ -0,0 +1,15 @@
+﻿using EspHomeTools.Interfaces;
+using EspHomeTools.Scratch.Interfaces.RenderManagers;
+
+namespace EspHomeTools.Classes;
+
+public class YamlString : IYamlString
+{
+    public void Render(IYamlRenderManager yamlRenderManager, int indentationLevel)
+    {
+        throw new System.NotImplementedException();
+    }
+    public string? Value { get; set; }
+    public bool IsValueNull => Value == null;
+    public bool HasValue => string.IsNullOrWhiteSpace(Value) == false;
+}
