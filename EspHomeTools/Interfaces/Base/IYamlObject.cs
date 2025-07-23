8e1e761e36565c300bde1f62768af9f849e5b6a9
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 13:57:19 +0200

    Add XML documentation to `IYamlObject` interface in `EspHomeTools.Interfaces.Base`.

--- EspHomeTools/Interfaces/Base/IYamlObject.cs
+++ EspHomeTools/Interfaces/Base/IYamlObject.cs
@@ -2,4 +2,11 @@
 
 namespace EspHomeTools.Interfaces.Base;
 
+/// <summary>
+/// Represents a base interface for objects that can be serialized into YAML format.
+/// </summary>
+/// <remarks>
+/// This interface extends the capabilities of <see cref="IYamlRenderable"/>
+/// by serving as the foundation for YAML object representation.
+/// </remarks>
 public interface IYamlObject : IYamlRenderable { }
