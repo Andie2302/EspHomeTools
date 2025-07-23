2245ce0ff7ff53c151f97c0c350f9339e447cfd4
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 03:10:06 +0200

    Remove `IYamlObject` interface as it is unused and redundant.

--- EspHomeTools/Interfaces/IYamlObject.cs
+++
@@ -1,9 +0,0 @@
-﻿using EspHomeTools.Classes;
-using EspHomeTools.Classes.Render;
-
-namespace EspHomeTools.Interfaces;
-
-public interface IYamlObject
-{
-    void Render(YamlRenderManager yamlRenderManager, int indentationLevel);
-}
