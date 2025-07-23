19eb14807868f215a103558324278e36522764ce
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 14:09:05 +0200

    Add `IYamlValue` interface to `EspHomeTools.Interfaces`.

---
+++ EspHomeTools/Interfaces/IYamlValue.cs
@@ -0,0 +1,9 @@
+﻿namespace EspHomeTools.Interfaces;
+
+public interface IYamlValue;
+
+public interface IYamlValue<T> : IYamlValue
+{
+    public T? Value { get; set; }
+    public bool HasValue { get; }
+}
