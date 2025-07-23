d31a11d9fc082c7048759e0e257fb4a9c2121609
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 22:43:40 +0200

    Refactor `Render` method in `YamlString` to improve readability and reuse with `GetRenderValue` and `EscapeString`.

--- EspHomeTools/Classes/YamlString.cs
+++ EspHomeTools/Classes/YamlString.cs
@@ -4,20 +4,17 @@ namespace EspHomeTools.Classes;
 
 public class YamlString : IYamlString
 {
-    public void Render(IYamlRenderManager yamlRenderManager, int indentationLevel)
-    {
-        if (!IsValueNull)
-        {
-            var value = Value ?? string.Empty;
-            var escapedValue = value.Replace("\\", @"\\").Replace("\"", @"\""");
-            yamlRenderManager.Append($"\"{escapedValue}\"", indentationLevel);
-            return;
-        }
-
-        yamlRenderManager.Append("null", indentationLevel);
-    }
-
+    private const string NullValue = "null";
+    private const string BackslashEscape = @"\\";
+    private const string QuoteEscape = "\\\"";
     public string? Value { get; set; }
     public bool IsValueNull => Value == null;
     public bool HasValue => string.IsNullOrWhiteSpace(Value) == false;
+    public void Render(IYamlRenderManager yamlRenderManager, int indentationLevel)
+    {
+        var renderValue = GetRenderValue();
+        yamlRenderManager.Append(renderValue, indentationLevel);
+    }
+    private string GetRenderValue() => IsValueNull ? NullValue : $"\"{EscapeString(Value ?? string.Empty)}\"";
+    private static string EscapeString(string input) => input.Replace("\\", BackslashEscape).Replace("\"", QuoteEscape);
 }
