91b39a5f503f3491b4b82e39f9cb438882cc01b9
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 14:49:32 +0200

    Add `YamlInteger` class implementing `IYamlInteger`.

---
+++ EspHomeTools/Classes/YamlInteger.cs
@@ -0,0 +1,13 @@
+﻿using EspHomeTools.Interfaces;
+using EspHomeTools.Scratch.Interfaces.RenderManagers;
+
+namespace EspHomeTools.Classes;
+
+public class YamlInteger : IYamlInteger
+{
+    public void Render(IYamlRenderManager yamlRenderManager, int indentationLevel)
+    {
+        throw new System.NotImplementedException();
+    }
+    public int Value { get; set; }
+}
