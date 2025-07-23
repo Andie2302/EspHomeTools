ec962a4cc55a4d3196f2ef408ebff9c51781b421
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 15:42:13 +0200

    Rename extension methods in `YamlRenderManagerExtensions` and add `[SuppressMessage]` attribute.

--- EspHomeTools/Classes/YamlRenderManagerExtensions.cs
+++ EspHomeTools/Classes/YamlRenderManagerExtensions.cs
@@ -1,33 +1,19 @@
-﻿using EspHomeTools.Interfaces;
+﻿using System.Diagnostics.CodeAnalysis;
+using EspHomeTools.Interfaces;
 
 namespace EspHomeTools.Classes;
 
-/// <summary>
-/// Enthält Erweiterungsmethoden für Klassen, die das IYamlRenderManager-Interface implementieren.
-/// </summary>
+[SuppressMessage("ReSharper", "UnusedType.Global")]
 public static class YamlRenderManagerExtensions
 {
-    /// <summary>
-    /// Fügt eine formatierte Key-Value-Zeile zum YAML-Output hinzu.
-    /// Beispiel: "  key: value"
-    /// </summary>
-    /// <param name="manager">Die Instanz des Render-Managers.</param>
-    /// <param name="key">Der Schlüssel, der geschrieben werden soll.</param>
-    /// <param name="value">Der Wert, der geschrieben werden soll.</param>
-    /// <param name="indentationLevel">Die Einrückungsebene.</param>
-    public static void AppendKeyValueLine(this IYamlRenderManager manager, string key, string value, int indentationLevel)
+    public static void ExAppendKeyValueLine(this IYamlRenderManager manager, string key, string value, int indentationLevel)
     {
-        // Benutzt die bestehenden Methoden des Managers
         manager.Append($"{key}: ", indentationLevel);
-        manager.Append(value, 0); // Wert ohne zusätzliche Einrückung anhängen
-        manager.AppendLine(); // Zeilenumbruch am Ende
+        manager.Append(value, 0);
+        manager.AppendLine();
     }
 
-    /// <summary>
-    /// Fügt eine YAML-Kommentarzeile hinzu.
-    /// Beispiel: "  # Das ist ein Kommentar"
-    /// </summary>
-    public static void AppendCommentLine(this IYamlRenderManager manager, string comment, int indentationLevel)
+    public static void ExAppendCommentLine(this IYamlRenderManager manager, string comment, int indentationLevel)
     {
         manager.AppendLine($"# {comment}", indentationLevel);
     }
