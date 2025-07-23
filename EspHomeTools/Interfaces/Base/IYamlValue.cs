3ac47d30a6ed6c14893e2e458b128ff59b107c34
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 13:48:20 +0200

    Add generic `IYamlValue<T>` interface to `EspHomeTools.Interfaces.Base`

--- EspHomeTools/Interfaces/Base/IYamlValue.cs
+++ EspHomeTools/Interfaces/Base/IYamlValue.cs
@@ -1,3 +1,9 @@
 ﻿namespace EspHomeTools.Interfaces.Base;
 
 public interface IYamlValue { }
+
+public interface IYamlValue<T> : IYamlValue
+{
+    T? Value { get; set; }
+    bool HasValue { get; }
+}
