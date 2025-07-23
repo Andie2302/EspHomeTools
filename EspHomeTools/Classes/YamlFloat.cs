8eabfb4586343a9eb0eae5ae89c8021b3250d312
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 14:49:27 +0200

    Remove unused YAML classes `YamlInteger`, `YamlString`, and `YamlNull`.

--- EspHomeTools/Classes/YamlFloat.cs
+++ EspHomeTools/Classes/YamlFloat.cs
@@ -11,33 +11,3 @@ public class YamlFloat : IYamlFloat
     }
     public float Value { get; set; }
 }
-
-public class YamlInteger : IYamlInteger
-{
-    public void Render(IYamlRenderManager yamlRenderManager, int indentationLevel)
-    {
-        throw new System.NotImplementedException();
-    }
-    public int Value { get; set; }
-}
-
-public class YamlString : IYamlString
-{
-    public void Render(IYamlRenderManager yamlRenderManager, int indentationLevel)
-    {
-        throw new System.NotImplementedException();
-    }
-    public string? Value { get; set; }
-    public bool IsValueNull => Value == null;
-    public bool HasValue => string.IsNullOrWhiteSpace(Value) == false;
-}
-public class YamlNull : IYamlNull, IYamlNullable
-{
-    public void Render(IYamlRenderManager yamlRenderManager, int indentationLevel)
-    {
-        throw new System.NotImplementedException();
-    }
-    public string? Value { get; set; } = null;
-    public bool IsValueNull => true;
-}
-
