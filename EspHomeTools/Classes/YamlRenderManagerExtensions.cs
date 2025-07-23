4cebf5c3c40c956b88a67a5cb79caa64018b3fa0
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 22:01:30 +0200

    Update `AppendComment` method in `YamlRenderManagerExtensions` to use `YamlRenderManager` instead of `IYamlRenderManager`.

--- EspHomeTools/Classes/YamlRenderManagerExtensions.cs
+++ EspHomeTools/Classes/YamlRenderManagerExtensions.cs
@@ -6,7 +6,7 @@ namespace EspHomeTools.Classes;
 [SuppressMessage("ReSharper", "UnusedType.Global")]
 public static class YamlRenderManagerExtensions
 {
-   public static void AppendComment(this IYamlRenderManager manager, string comment, int indentationLevel,bool linebreak)
+   public static void AppendComment(this YamlRenderManager manager, string comment, int indentationLevel,bool linebreak)
    {
       if (linebreak)
       {
