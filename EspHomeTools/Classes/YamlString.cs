153d01ed4303b9684df601e66eb2db0aa243c3b1
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 23:26:09 +0200

    Refactor `YamlString` to inherit from `YamlScalarBase<string?>` for improved simplicity and reuse.

--- EspHomeTools/Classes/YamlString.cs
+++ EspHomeTools/Classes/YamlString.cs
@@ -2,10 +2,8 @@
 
 namespace EspHomeTools.Classes;
 
-public class YamlString : IYamlString
+public class YamlString : YamlScalarBase<string?>, IYamlString
 {
-    public string? Value { get; set; }
     public bool IsValueNull => Value == null;
     public bool HasValue => string.IsNullOrWhiteSpace(Value) == false;
-    public void Render(IYamlRenderManager yamlRenderManager, int indentationLevel) => yamlRenderManager.AppendScalar(this, indentationLevel);
 }
