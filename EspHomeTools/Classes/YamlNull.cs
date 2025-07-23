884d8a35a18996a28190202a221ff40b392042d9
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 14:49:39 +0200

    Add `YamlNull` class implementing `IYamlNull` and `IYamlNullable`

---
+++ EspHomeTools/Classes/YamlNull.cs
@@ -0,0 +1,14 @@
+﻿using EspHomeTools.Interfaces;
+using EspHomeTools.Scratch.Interfaces.RenderManagers;
+
+namespace EspHomeTools.Classes;
+
+public class YamlNull : IYamlNull, IYamlNullable
+{
+    public void Render(IYamlRenderManager yamlRenderManager, int indentationLevel)
+    {
+        throw new System.NotImplementedException();
+    }
+    public string? Value { get; set; } = null;
+    public bool IsValueNull => true;
+}
