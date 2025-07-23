a37e1efe51b0279cfadbd33276cbb504b0973a9f
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 23:42:02 +0200

    Remove unused `IYamlString` interface from `YamlString` definition for cleaner implementation.

--- EspHomeTools/Classes/YamlString.cs
+++ EspHomeTools/Classes/YamlString.cs
@@ -2,7 +2,7 @@
 
 namespace EspHomeTools.Classes;
 
-public class YamlString : YamlScalarBase<string?>, IYamlString
+public class YamlString : YamlScalarBase<string?>
 {
     public bool IsValueNull => Value == null;
     public bool HasValue => string.IsNullOrWhiteSpace(Value) == false;
