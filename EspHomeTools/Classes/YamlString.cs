0d6fd57b3e7da3e6800edd11a338d3111e9b4498
Andreas <Andreas.Zwander@gmail.com>
2025-07-23 22:25:57 +0200

    Implement `Render` method in `YamlString` to format and append value using `IYamlRenderManager`.

--- EspHomeTools/Classes/YamlString.cs
+++ EspHomeTools/Classes/YamlString.cs
@@ -6,8 +6,17 @@ public class YamlString : IYamlString
 {
     public void Render(IYamlRenderManager yamlRenderManager, int indentationLevel)
     {
-        throw new System.NotImplementedException();
+        if (!IsValueNull)
+        {
+            var value = Value ?? string.Empty;
+            var escapedValue = value.Replace("\\", @"\\").Replace("\"", @"\""");
+            yamlRenderManager.Append($"\"{escapedValue}\"", indentationLevel);
+            return;
+        }
+
+        yamlRenderManager.Append("null", indentationLevel);
     }
+
     public string? Value { get; set; }
     public bool IsValueNull => Value == null;
     public bool HasValue => string.IsNullOrWhiteSpace(Value) == false;
