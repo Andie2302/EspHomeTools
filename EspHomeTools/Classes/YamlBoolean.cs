d4ee06e4f7f96e34c1ec27b74733c55a12f96dde
Andreas <Andreas.Zwander@gmail.com>
2025-07-24 16:47:04 +0200

    Add `YamlBoolean` class with `IYamlBoolean` implementation and comment support

---
+++ EspHomeTools/Classes/YamlBoolean.cs
@@ -0,0 +1,15 @@
+﻿using EspHomeTools.Classes.Comment;
+using EspHomeTools.Classes.Render;
+using EspHomeTools.Interfaces;
+
+namespace EspHomeTools.Classes;
+
+public class YamlBoolean : IYamlBoolean
+{
+    public void Render(IYamlRenderer renderer, IYamlIndentationLevel indentationLevel)
+    {
+        throw new System.NotImplementedException();
+    }
+    public IYamlComment Comment { get; }
+    public YamlBoolean() => Comment = new YamlComment();
+}
