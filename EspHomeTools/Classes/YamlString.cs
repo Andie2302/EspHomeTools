5a375477679ba04295958acf084891553dd60c79
Andreas <Andreas.Zwander@gmail.com>
2025-07-24 03:26:31 +0200

    Remove unused `YamlString` class to clean up codebase.

--- EspHomeTools/Classes/YamlString.cs
+++
@@ -1,9 +0,0 @@
-﻿using EspHomeTools.Interfaces;
-
-namespace EspHomeTools.Classes;
-
-public class YamlString : YamlScalarBase<string?>
-{
-    public bool IsValueNull => Value == null;
-    public bool HasValue => string.IsNullOrWhiteSpace(Value) == false;
-}
