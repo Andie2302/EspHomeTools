b4157435d7740a1ed3abc2d92bcc6b7e877f3f9b
Andreas <Andreas.Zwander@gmail.com>
2025-07-24 03:25:29 +0200

    Add `IYamlStructure` interface for YAML block structure representation.

---
+++ EspHomeTools/Interfaces/IYamlStructure.cs
@@ -0,0 +1,9 @@
+﻿namespace EspHomeTools.Interfaces;
+
+public interface IYamlStructure : IYamlObject
+{
+    string? BlockName { get; set; }
+    bool HasBlockName { get; }
+}
+
+public interface IYamlStructure<T> : IYamlStructure { }
