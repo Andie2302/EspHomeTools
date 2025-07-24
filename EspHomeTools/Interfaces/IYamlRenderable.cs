9e6a555ceb1c60abdba65ede2bc68b0c6539ff28
Andreas <Andreas.Zwander@gmail.com>
2025-07-24 03:24:56 +0200

    Update `IYamlRenderable` method signature for improved parameter clarity.

--- EspHomeTools/Interfaces/IYamlRenderable.cs
+++ EspHomeTools/Interfaces/IYamlRenderable.cs
@@ -1,14 +1,6 @@
 ﻿namespace EspHomeTools.Interfaces;
 
-/// <summary>
-/// Represents an interface defining the ability of an object to render its state into a YAML representation.
-/// </summary>
 public interface IYamlRenderable
 {
-    /// <summary>
-    /// Renders the YAML representation of an object using the specified render manager and indentation level.
-    /// </summary>
-    /// <param name="yamlRenderManager">The render manager utilized for generating YAML content in a formatted manner.</param>
-    /// <param name="indentationLevel">The level of indentation applied during the rendering process.</param>
-    void Render(IYamlRenderManager yamlRenderManager, int indentationLevel);
+    void Render(IYamlRenderer renderer, IYamlIndentationLevel indentationLevel);
 }
