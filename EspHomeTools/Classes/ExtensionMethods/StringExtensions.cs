1544dfc07d3e46eb4fa56db5cfa72c57fc7bbbbf
Andreas <Andreas.Zwander@gmail.com>
2025-07-24 16:46:58 +0200

    Add `StringExtensions` with methods for replacing and handling German umlauts

---
+++ EspHomeTools/Classes/ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Text;
+namespace EspHomeTools.Classes.ExtensionMethods;
+public static class StringExtensions
+{
+    private const int StringBuilderCapacityMultiplier = 2;
+    private readonly static Dictionary<char, string> GermanUmlautReplacements = new()
+    {
+        { 'Ä', "ae" }, { 'ä', "ae" },
+        { 'Ö', "oe" }, { 'ö', "oe" },
+        { 'Ü', "ue" }, { 'ü', "ue" },
+        { 'ß', "ss" }
+    };
+
+    public static string ReplaceUmlauts(this string input)
+    {
+        if (string.IsNullOrEmpty(input)) return input;
+        var result = new StringBuilder(input.Length * StringBuilderCapacityMultiplier);
+        foreach (var character in input)
+        {
+            if (GermanUmlautReplacements.TryGetValue(character, out var replacement))
+            {
+                result.Append(replacement);
+            }
+            else
+            {
+                result.Append(character);
+            }
+        }
+        return result.ToString();
+    }
+    public static string ToLowerCaseWithoutUmlauts(this string input)
+    {
+        return input.ReplaceUmlauts().ToLowerInvariant();
+    }
+}
