b2dc6d788112586d2aad4e08b3151f87e9b02236
Andreas <Andreas.Zwander@gmail.com>
2025-07-24 04:30:43 +0200

    Add `IYamlComment` interface for managing YAML comments

---
+++ EspHomeTools/Classes/YamlObject.cs
@@ -0,0 +1,16 @@
+﻿namespace EspHomeTools.Classes;
+
+public interface IYamlComment
+{
+    void SetInline(string comment);
+    void AddAbove(string comment);
+    void AddBelow(string comment);
+    void ClearAbove();
+    void ClearBelow();
+    void ClearInline();
+    void Clear();
+    bool HasInline { get; }
+    bool HasAbove { get; }
+    bool HasBelow { get; }
+    bool HasAny { get; }
+}
