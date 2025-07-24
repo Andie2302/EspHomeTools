ecc41cbcba41cfff5d3ce7c855dcb54202071612
Andreas <Andreas.Zwander@gmail.com>
2025-07-24 03:23:55 +0200

    Add `IYamlIndentationLevel` interface for YAML indentation management.

---
+++ EspHomeTools/Interfaces/IYamlIndentationLevel.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+namespace EspHomeTools.Interfaces;
+
+public interface IYamlIndentationLevel
+{
+    int IndentationLevel { get; set; }
+    int IndentationMultiplier { get; set; }
+    int Count();
+    public abstract string GetIndentation();
+    public abstract void IncreaseIndentationLevel();
+    public abstract void DecreaseIndentationLevel();
+    public abstract void AppendIndentation(StringBuilder stringBuilder);
+}
