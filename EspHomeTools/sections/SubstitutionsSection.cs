231ed094c717018324f1504f04571030fad3ce6b
Andreas <Andreas.Zwander@gmail.com>
2026-01-10 15:24:20 +0100

    Add SubstitutionsSection to manage substitutions in EspHomeTools

---
+++ EspHomeTools/sections/SubstitutionsSection.cs
@@ -0,0 +1,8 @@
+﻿using EspHomeTools.values;
+
+namespace EspHomeTools.sections;
+
+public class SubstitutionsSection
+{
+    public Dictionary<string, EspHomeValue> Substitutions { get; init; } = new();
+}
