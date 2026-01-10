6d16c3b891d3576f7485368e010b02617a59a2b1
Andreas <Andreas.Zwander@gmail.com>
2026-01-10 14:45:49 +0100

    Update Program.cs to generate and serialize an Esphome configuration using YamlDotNet

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,3 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using EspHomeTools;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
 Console.WriteLine("Hello, World!");
+
+var serializer = new SerializerBuilder()
+    .WithNamingConvention(UnderscoredNamingConvention.Instance) // ESPHome nutzt snake_case
+    .Build();
+
+var myConfig = new EsphomeDevice 
+{
+    SectionEsphome = new() { Name = "test_node" },
+    Substitutions = new()
+    {
+        ["name"] = "og-sz-bett",
+        ["friendly_name"] = "OG-SZ-BETT",
+        ["wifi_ssid"] = new EsphomeValue("wifi_ssid", true),
+        ["wifi_password"] = new EsphomeValue("wifi_password", true),
+        ["ap_ssid"] = new EsphomeValue("ap_ssid", true),
+        ["ap_password"] = new EsphomeValue("ap_password", true),
+        
+    },
+    Sensors = [
+        new() { ["platform"] = "dht", ["pin"] = 14, ["name"] = "Temp" }
+    ]
+};
+
+string yamlOutput = serializer.Serialize(myConfig);
+Console.WriteLine(yamlOutput);
