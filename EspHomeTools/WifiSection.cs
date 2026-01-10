47258370b2079e4b2cf6939ee8b53c5c04e860ed
Andreas <Andreas.Zwander@gmail.com>
2026-01-10 15:04:50 +0100

    Update WifiSection defaults to use template variables

--- EspHomeTools/WifiSection.cs
+++ EspHomeTools/WifiSection.cs
@@ -2,7 +2,7 @@
 
 public record WifiSection
 {
-    public string Ssid { get; init; } = string.Empty;
-    public string Password { get; init; } = string.Empty;
+    public string Ssid { get; init; } =  "${wifi_ssid}";
+    public string Password { get; init; } =  "${wifi_password}";
     public ApSection? Ap { get; init; } = new ApSection();
 }
