919e4040aca3856ccf07aee5e85c217d19c4f385
Andreas <Andreas.Zwander@gmail.com>
2026-01-10 18:10:53 +0100

    Add hardware platform-specific section definitions to `WifiSection`

--- EspHomeTools/sections/WifiSection.cs
+++ EspHomeTools/sections/WifiSection.cs
@@ -6,3 +6,24 @@ public record WifiSection
     public string Password { get; init; } =  "${wifi_password}";
     public ApSection? Ap { get; init; } = new();
 }
+
+
+public abstract record PlatformSectionBase
+{
+    public string Board { get; init; } = string.Empty;
+    public string? Framework { get; init; } // Optional: arduino oder esp-idf
+}
+
+
+public record Esp32Section : PlatformSectionBase { }
+public record Esp32S2Section : PlatformSectionBase { }
+public record Esp32S3Section : PlatformSectionBase { }
+public record Esp32C3Section : PlatformSectionBase { }
+public record Esp32C6Section : PlatformSectionBase { }
+public record Esp8266Section : PlatformSectionBase { }
+public record Rp2040 : PlatformSectionBase { }
+public record Rtl87Xx : PlatformSectionBase { }
+public record Bk72Xx : PlatformSectionBase {}
+public record Ln882X : PlatformSectionBase {}
+public record RaspBerryPicoW : PlatformSectionBase { }
+
