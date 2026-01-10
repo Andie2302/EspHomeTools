93aece9c6b12eaf785a5fe08eb2e5f1bac51dc78
Andreas <Andreas.Zwander@gmail.com>
2026-01-10 15:23:56 +0100

    Rename EsphomeSection to EspHomeSection to align with naming conventions

--- EspHomeTools/sections/EspHomeSection.cs
+++ EspHomeTools/sections/EspHomeSection.cs
@@ -1,6 +1,6 @@
 ﻿namespace EspHomeTools.sections;
 
-public record EsphomeSection
+public record EspHomeSection
 {
     public string Name { get; init; } = "default-node";
     public string FriendlyName { get; init; } = string.Empty;
