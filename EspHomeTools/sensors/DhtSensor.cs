36cde4e21840bbeb2753fcbe5bf2ccfbfc17b796
Andreas <Andreas.Zwander@gmail.com>
2026-01-10 16:17:24 +0100

    Add `DhtSensor` record with `Pin` and `Model` properties for DHT platform configuration

---
+++ EspHomeTools/sensors/DhtSensor.cs
@@ -0,0 +1,9 @@
+﻿namespace EspHomeTools.sensors;
+
+public record DhtSensor : SensorBase
+{
+    public override string Platform => "dht";
+
+    public int Pin { get; init; }
+    public string? Model { get; init; }
+}
