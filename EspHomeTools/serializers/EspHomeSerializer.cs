7f5813c77de5dcfdec1b825d4fc991ea08f61779
Andreas <Andreas.Zwander@gmail.com>
2026-01-10 15:14:50 +0100

    Add EspHomeSerializer for YAML serialization of EsphomeDevice objects

---
+++ EspHomeTools/serializers/EspHomeSerializer.cs
@@ -0,0 +1,32 @@
+﻿using EspHomeTools.devices;
+using EspHomeTools.serializers.converters;
+using EspHomeTools.values;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace EspHomeTools.serializers;
+
+public class EspHomeSerializer
+{
+    private readonly ISerializer _serializer;
+
+    public EspHomeSerializer()
+    {
+        _serializer = new SerializerBuilder()
+            .WithNamingConvention(UnderscoredNamingConvention.Instance)
+            .WithTypeConverter(new EsphomeValueConverter())
+            .ConfigureDefaultValuesHandling(
+                DefaultValuesHandling.OmitDefaults | 
+                DefaultValuesHandling.OmitEmptyCollections | 
+                DefaultValuesHandling.OmitNull)
+            .Build();
+    }
+
+    /// <summary>
+    /// Serialisiert ein EsphomeDevice Objekt in einen YAML-String.
+    /// </summary>
+    public string Serialize(EsphomeDevice device)
+    {
+        return _serializer.Serialize(device);
+    }
+}
