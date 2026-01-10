3d9f803293a06f4152babf7594e450c6f1102d92
Andreas <Andreas.Zwander@gmail.com>
2026-01-10 16:17:29 +0100

    Update `EspHomeDevice` to add new sections (`Esp32`, `Logger`, `Ota`, `WebServer`, `CaptivePortal`) with proper serialization order

--- EspHomeTools/devices/EspHomeDevice.cs
+++ EspHomeTools/devices/EspHomeDevice.cs
@@ -1,21 +1,39 @@
-﻿using EspHomeTools.sections;
-using EspHomeTools.sensors;
-using EspHomeTools.values;
-using YamlDotNet.Serialization;
-
+﻿// EspHomeTools/devices/EspHomeDevice.cs
 namespace EspHomeTools.devices;
+
+using sections;
+using sensors;
+using YamlDotNet.Serialization;
 
 public record EspHomeDevice
 {
-    [YamlMember(Alias = "esphome", Order = 0)]
+    [YamlMember(Alias = "substitutions", Order = 1)]
+    public SubstitutionsSection Substitutions { get; init; } = new();
+
+    [YamlMember(Alias = "esphome", Order = 2)]
     public EspHomeSection SectionEspHome { get; init; } = new();
 
-    [YamlMember(Alias = "substitutions", Order = -1)]
-    public Dictionary<string, EspHomeValue> Substitutions { get; init; } = [];
+    [YamlMember(Alias = "esp32", Order = 3)] // Falls du ein Board spezifizierst
+    public Dictionary<string, object>? Esp32 { get; init; }
 
-    [YamlMember(Alias = "wifi", Order = 1)]
-    public WifiSection? Wifi { get; init; } = new();
+    [YamlMember(Alias = "logger", Order = 10)]
+    public LoggerSection? Logger { get; init; } = new(); // Standardmäßig aktiv
 
-    [YamlMember(Alias = "sensor", Order = 4)]
+    [YamlMember(Alias = "api", Order = 11)]
+    public ApiSection? Api { get; init; }
+
+    [YamlMember(Alias = "ota", Order = 12)]
+    public OtaSection? Ota { get; init; }
+
+    [YamlMember(Alias = "web_server", Order = 13)]
+    public WebServerSection? WebServer { get; init; }
+
+    [YamlMember(Alias = "wifi", Order = 20)]
+    public WifiSection? Wifi { get; init; }
+
+    [YamlMember(Alias = "captive_portal", Order = 21)]
+    public CaptivePortalSection? CaptivePortal { get; init; }
+
+    [YamlMember(Alias = "sensor", Order = 30)]
     public List<SensorBase> Sensors { get; init; } = [];
 }
