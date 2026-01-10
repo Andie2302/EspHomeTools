1c25c8cbd5ce1661568e27c3f4a4cd9ebe2a4f16
Andreas <Andreas.Zwander@gmail.com>
2026-01-10 14:56:52 +0100

    Remove unused records and YAML dependencies to simplify EsphomeValue model

---
+++ EspHomeTools/EsphomeValue.cs
@@ -0,0 +1,16 @@
+﻿namespace EspHomeTools;
+
+public record EsphomeValue
+{
+    public object? Value { get; init; }
+    public bool IsSecret { get; init; }
+
+    public static implicit operator EsphomeValue(string s) => new(s,false) { Value = s };
+    public static implicit operator EsphomeValue(int i) => new(i,false) { Value = i };
+
+    public EsphomeValue(object value, bool isSecret)
+    {
+        Value = value;
+        IsSecret = isSecret;
+    }
+}
