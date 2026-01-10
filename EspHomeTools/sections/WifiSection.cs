925025d3723b8b9977b09dd7ebc52380a190e4ab
Andreas <Andreas.Zwander@gmail.com>
2026-01-10 19:32:49 +0100

    Initialize hardware platform sections with default values in `WifiSection`

--- EspHomeTools/sections/WifiSection.cs
+++ EspHomeTools/sections/WifiSection.cs
@@ -8,22 +8,109 @@ public record WifiSection
 }
 
 
-public abstract record PlatformSectionBase
+public record PlatformSectionBase
 {
     public string Board { get; init; } = string.Empty;
     public string? Framework { get; init; } // Optional: arduino oder esp-idf
 }
 
 
-public record Esp32Section : PlatformSectionBase { }
-public record Esp32S2Section : PlatformSectionBase { }
-public record Esp32S3Section : PlatformSectionBase { }
-public record Esp32C3Section : PlatformSectionBase { }
-public record Esp32C6Section : PlatformSectionBase { }
-public record Esp8266Section : PlatformSectionBase { }
-public record Rp2040 : PlatformSectionBase { }
-public record Rtl87Xx : PlatformSectionBase { }
-public record Bk72Xx : PlatformSectionBase {}
-public record Ln882X : PlatformSectionBase {}
-public record RaspBerryPicoW : PlatformSectionBase { }
+public record Esp32Section : PlatformSectionBase
+{
+    public Esp32Section()
+    {
+        Board = "esp32dev";
+        Framework = "esp-idf";
+    }
+}
+
+public record Esp32S2Section : PlatformSectionBase
+{
+    public Esp32S2Section()
+    {
+        Board = "esp32-s2-saola-1";
+        Framework = "esp-idf";
+    }
+}
+
+public record Esp32S3Section : PlatformSectionBase
+{
+    public Esp32S3Section()
+    {
+        Board = "esp32-s3-devkitc-1";
+        Framework = "esp-idf";
+    }
+}
+
+public record Esp32C3Section : PlatformSectionBase
+{
+    public Esp32C3Section()
+    {
+        Board = "esp32-c3-devkitm-1";
+        Framework = "esp-idf";
+    }
+}
+
+public record Esp32C6Section : PlatformSectionBase
+{
+    public Esp32C6Section()
+    {
+        Board = "esp32-c6-devkitc-1";
+        Framework = "esp-idf";
+    }
+}
+
+public record Esp8266Section : PlatformSectionBase
+{
+    public Esp8266Section()
+    {
+        Board = "esp01_1m";
+        Framework = null;
+    }
+}
+
+public record Rp2040 : PlatformSectionBase
+{
+    public Rp2040()
+    {
+        Board = "rp2040";
+        Framework = null;
+    }
+}
+
+public record Rtl87Xx : PlatformSectionBase
+{
+    public Rtl87Xx()
+    {
+        Board = "bw12";
+        Framework = null;
+    }
+}
+
+public record Bk72Xx : PlatformSectionBase
+{
+    public Bk72Xx()
+    {
+        Board = "cb1s";
+        Framework = null;
+    }
+}
+
+public record Ln882X : PlatformSectionBase
+{
+    public Ln882X()
+    {
+        Board = "generic-ln882hki"; // ln-02   wl2s
+        Framework = null;
+    }
+}
+
+public record RaspBerryPicoW: PlatformSectionBase
+{
+    public RaspBerryPicoW()
+    {
+        Board = "rpipicow";
+        Framework = null;
+    }
+}
 
