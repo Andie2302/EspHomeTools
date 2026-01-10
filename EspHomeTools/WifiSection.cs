13b54be5fee83ef3c9c5a5fd804fadd3d9838c93
Andreas <Andreas.Zwander@gmail.com>
2026-01-10 14:57:11 +0100

    Add WifiSection record for Wi-Fi configuration modeling

---
+++ EspHomeTools/WifiSection.cs
@@ -0,0 +1,8 @@
+﻿namespace EspHomeTools;
+
+public record WifiSection
+{
+    public string Ssid { get; init; } = string.Empty;
+    public string Password { get; init; } = string.Empty;
+    public ApSection? Ap { get; init; } = new ApSection();
+}
