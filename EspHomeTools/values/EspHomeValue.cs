b5893a71438856338965f7456d8002dcf25705f9
Andreas <Andreas.Zwander@gmail.com>
2026-01-10 15:24:05 +0100

    Rename EsphomeValue to EspHomeValue for consistency with naming conventions

---
+++ EspHomeTools/values/EspHomeValue.cs
@@ -0,0 +1,16 @@
+﻿namespace EspHomeTools.values;
+
+public record EspHomeValue
+{
+    public object? Value { get; init; }
+    public bool IsSecret { get; init; }
+
+    public static implicit operator EspHomeValue(string s) => new(s,false) { Value = s };
+    public static implicit operator EspHomeValue(int i) => new(i,false) { Value = i };
+
+    public EspHomeValue(object value, bool isSecret)
+    {
+        Value = value;
+        IsSecret = isSecret;
+    }
+}
