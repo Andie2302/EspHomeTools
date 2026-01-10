dd344cbd08f1bc56181ec31e70cc4258963ad3d6
Andreas <Andreas.Zwander@gmail.com>
2026-01-10 14:56:36 +0100

    Add EsphomeDevice record with YAML-mapped properties

---
+++ EspHomeTools/EsphomeDevice.cs
@@ -0,0 +1,18 @@
+﻿using YamlDotNet.Serialization;
+
+namespace EspHomeTools;
+
+public record EsphomeDevice
+{
+    [YamlMember(Alias = "esphome")]
+    public EsphomeSection SectionEsphome { get; init; } = new();
+
+    [YamlMember(Alias = "substitutions")]
+    public Dictionary<string, EsphomeValue> Substitutions { get; init; } = [];
+
+    [YamlMember(Alias = "wifi")]
+    public WifiSection Wifi { get; init; } = new WifiSection();
+
+    [YamlMember(Alias = "sensor")]
+    public List<Dictionary<string, object>> Sensors { get; init; } = [];
+}
