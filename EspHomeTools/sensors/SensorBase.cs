356818044aaa2a1b7588e731d5b3b06bdd311624
Andreas <Andreas.Zwander@gmail.com>
2026-01-10 16:09:58 +0100

    Add `SensorBase` abstract record and derived sensor types (`DhtSensor`, `UptimeSensor`) for type-safe sensor configuration

---
+++ EspHomeTools/sensors/SensorBase.cs
@@ -0,0 +1,30 @@
+﻿
+using YamlDotNet.Serialization;
+
+namespace EspHomeTools.sensors;
+
+public abstract record SensorBase
+{
+    [YamlMember(Order = -1)] // Plattform immer ganz oben
+    public abstract string Platform { get; }
+
+    public string? Name { get; init; }
+    
+    [YamlMember(Alias = "update_interval")]
+    public string? UpdateInterval { get; init; }
+}
+
+
+public record DhtSensor : SensorBase
+{
+    public override string Platform => "dht";
+
+    public int Pin { get; init; }
+    public string? Model { get; init; } // z.B. "DHT22"
+}
+
+
+public record UptimeSensor : SensorBase
+{
+    public override string Platform => "uptime";
+}
