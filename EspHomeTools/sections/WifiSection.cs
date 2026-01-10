93aa0b793f8d69044d7b38dc695044e1ed6cafac
Andreas <Andreas.Zwander@gmail.com>
2026-01-10 16:09:52 +0100

    Simplify object initialization for `ApSection` in `WifiSection`.

--- EspHomeTools/sections/WifiSection.cs
+++ EspHomeTools/sections/WifiSection.cs
@@ -4,5 +4,5 @@ public record WifiSection
 {
     public string Ssid { get; init; } =  "${wifi_ssid}";
     public string Password { get; init; } =  "${wifi_password}";
-    public ApSection? Ap { get; init; } = new ApSection();
+    public ApSection? Ap { get; init; } = new();
 }
