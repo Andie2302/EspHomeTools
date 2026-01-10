a852902f0bae10f71516916d92ea7fccd8abc8fd
Andreas <Andreas.Zwander@gmail.com>
2026-01-10 14:56:41 +0100

    Add EsphomeSection record with default properties for configuration modeling

---
+++ EspHomeTools/EsphomeSection.cs
@@ -0,0 +1,7 @@
+﻿namespace EspHomeTools;
+
+public record EsphomeSection
+{
+    public string Name { get; init; } = "default-node";
+    public string FriendlyName { get; init; } = string.Empty;
+}
